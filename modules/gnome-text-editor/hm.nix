7d5b54a55bc3758fac47f7517a7ca352602a76f3
bricked <git@bricked.dev>
2025-03-11 18:39:41 +0100

    gtksourceview: fix warning condition

--- modules/gnome-text-editor/hm.nix
+++ modules/gnome-text-editor/hm.nix
@@ -1,4 +1,4 @@
-{ options, lib,mkTarget, ... }:
+{ config, lib,mkTarget, ... }:
 mkTarget {
   name = "gnome-text-editor";
   humanName = "GNOME Text Editor";
@@ -6,7 +6,7 @@ mkTarget {
   configElements = {
     dconf.settings."org/gnome/TextEditor".style-scheme = "stylix";
     warnings =
-      lib.optional (!options.stylix.targets.gtksourceview.enable)
+      lib.optional (!config.stylix.targets.gtksourceview.enable)
         "stylix: gnome-text-editor: This module will probably not work because the `gtksourceview' target is not enabled.";
   };
 }
