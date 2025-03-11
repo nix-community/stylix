819b6b0cd769eb6c5ee118326c0cd6fea07c6294
bricked <git@bricked.dev>
2025-03-11 20:48:50 +0100

    steam: symlink adwaita theme

--- flake.nix
+++ flake.nix
@@ -12,6 +12,11 @@
     systems.url = "github:nix-systems/default";
 
     # keep-sorted start block=yes newline_separated=yes
+    adwaita-for-steam = {
+      flake = false;
+      url = "github:tkashkin/Adwaita-for-Steam";
+    };
+
     base16-fish = {
       url = "github:tomyun/base16-fish";
       flake = false;
