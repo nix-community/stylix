468379576bf2f34807e4db187e1f3acc90c2bb62
Osi Bluber <87434959+OsiPog@users.noreply.github.com>
2025-06-20 16:19:21 +0200

    nixos-icons: modify `nix-snowflake-colours.svg` to match color scheme

--- modules/nixos-icons/overlay.nix
+++ modules/nixos-icons/overlay.nix
@@ -19,11 +19,18 @@
             prePatch = with config.lib.stylix.colors; ''
               substituteInPlace logo/nix-snowflake-white.svg --replace-fail '#ffffff' '#${base05}'
 
+              # The normal snowflake uses 2 gradients, replace each bluish color with blue and each light-blueish color with cyan
+              for color in '#699ad7' '#7eb1dd' '#7ebae4'; do
+                substituteInPlace logo/nix-snowflake-colours.svg --replace-fail $color '#${base0C}'
+              done
+              for color in '#415e9a' '#4a6baf' '#5277c3'; do
+                substituteInPlace logo/nix-snowflake-colours.svg --replace-fail $color '#${base0D}'
+              done
+
               # Insert attribution comment after the XML prolog
-              sed \
-                --in-place \
-                '2i<!-- The original NixOS logo from ${oldAttrs.src.url} is licensed under https://creativecommons.org/licenses/by/4.0 and has been modified to match the ${scheme} color scheme. -->' \
-                logo/nix-snowflake-white.svg
+              attribution='2i<!-- The original NixOS logo from ${oldAttrs.src.url} is licensed under https://creativecommons.org/licenses/by/4.0 and has been modified to match the ${scheme} color scheme. -->'
+              sed --in-place "$attribution" logo/nix-snowflake-colours.svg
+              sed --in-place "$attribution" logo/nix-snowflake-white.svg
             '';
           };
         });
