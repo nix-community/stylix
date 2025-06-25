nix-community/stylix

commit c765b15fc3949965126db8ca5b4b2b629b2034d2

stylix: move testbed to a dedicated directory

flake/packages.nix
@@ -17,7 +17,7 @@
         # Testbeds are virtual machines based on NixOS, therefore they are
         # only available for Linux systems.
         (lib.mkIf pkgs.stdenv.hostPlatform.isLinux (
-          import "${self}/stylix/testbed.nix" {
+          import "${self}/stylix/testbed/default.nix" {
             inherit pkgs inputs lib;
           }
         ))

.github/labeler.yml
@@ -50,5 +50,5 @@
 "topic: testbed":
   - changed-files:
       - any-glob-to-any-file:
-          - "stylix/testbed.nix"
+          - "stylix/testbed/**"
           - "modules/testbed/**"

commit 415a01b1c01ada6d6068c957dc9c08f995b486be

flake: infer default.nix import path
Fixes: c765b15fc394 ("stylix: move testbed to a dedicated directory")

flake/packages.nix
@@ -15,7 +15,7 @@
         # Testbeds are virtual machines based on NixOS, therefore they are
         # only available for Linux systems.
         (lib.mkIf pkgs.stdenv.hostPlatform.isLinux (
-          import ../stylix/testbed/default.nix {
+          import ../stylix/testbed {
             inherit pkgs inputs lib;
           }
         ))