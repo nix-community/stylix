8b69a14f963d4f5945cc4ca8ec679e31b8a31532
Devin Droddy <devin.droddy@gmail.com>
2025-06-27 23:10:50 -0400

    ashell: update option names for ashell v0.5.0

--- modules/ashell/hm.nix
+++ modules/ashell/hm.nix
@@ -9,14 +9,14 @@ mkTarget {
     { colors }:
     {
       programs.ashell.settings.appearance = with colors.withHashtag; {
-        backgroundColor = base00;
-        primaryColor = base0D;
-        secondaryColor = base01;
-        successColor = base0B;
-        dangerColor = base09;
-        textColor = base05;
+        background_color = base00;
+        primary_color = base0D;
+        secondary_color = base01;
+        success_color = base0B;
+        danger_color = base09;
+        text_color = base05;
 
-        workspaceColors = [
+        workspace_colors = [
           base09
           base0D
         ];
