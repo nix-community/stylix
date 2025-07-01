f50448f9ec43acaf72fc85f90a3b3e8ad7c7e90b
Devin Droddy <devin.droddy@gmail.com>
2025-06-30 21:38:15 -0400

    ashell: improve use of opacity option

--- modules/ashell/hm.nix
+++ modules/ashell/hm.nix
@@ -1,26 +1,35 @@
-{ mkTarget, config, ... }:
+{ mkTarget, ... }:
 mkTarget {
   name = "ashell";
   humanName = "Ashell";
 
-  configElements =
-    { colors }:
-    {
-      programs.ashell.settings.appearance = with colors.withHashtag; {
-        background_color = base00;
-        primary_color = base0D;
-        secondary_color = base01;
-        success_color = base0B;
-        danger_color = base09;
-        text_color = base05;
+  configElements = [
+    (
+      { colors }:
+      {
+        programs.ashell.settings.appearance = with colors.withHashtag; {
+          background_color = base00;
+          primary_color = base0D;
+          secondary_color = base01;
+          success_color = base0B;
+          danger_color = base09;
+          text_color = base05;
 
-        workspace_colors = [
-          base09
-          base0D
-        ];
-
-        opacity = config.stylix.opacity.desktop;
-        menu.opacity = config.stylix.opacity.desktop;
-      };
-    };
+          workspace_colors = [
+            base09
+            base0D
+          ];
+        };
+      }
+    )
+    (
+      { opacity }:
+      {
+        prrograms.ashell.settings.appearance = {
+          inherit opacity;
+          menu.opacity = opacity;
+        };
+      }
+    )
+  ];
 }
