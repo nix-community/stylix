5b8823a6cfe296d2fbd5af64382121318842606b
bricked <git@bricked.dev>
2025-07-05 14:34:57 +0200

    gnome-text-editor: remove warning because of unguarded config.stylix error

--- modules/gnome-text-editor/hm.nix
+++ modules/gnome-text-editor/hm.nix
@@ -1,18 +1,9 @@
-{ config, osConfig, lib ,mkTarget, ... }:
+{ mkTarget, ... }:
 mkTarget {
   name = "gnome-text-editor";
   humanName = "GNOME Text Editor";
 
   configElements = {
     dconf.settings."org/gnome/TextEditor".style-scheme = "stylix";
-    warnings =
-      lib.optional
-        (
-          !(builtins.any (c: c.stylix.targets.gtksourceview.enable) [
-            config
-            osConfig
-          ])
-        )
-        "stylix: gnome-text-editor: This module will probably not work because the `gtksourceview' target is not enabled.";
   };
 }
