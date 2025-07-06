mightyiam/infra

commit dea0337e0bffeeeb941ca6caffb44e966b13a97b

stylix: restrict access to config while using mkTarget (#1368)
Link: https://github.com/nix-community/stylix/pull/1368

Reviewed-by: Flameopathic <64027365+Flameopathic@users.noreply.github.com>
Reviewed-by: Matt Sturgeon <matt@sturgeon.me.uk>
Reviewed-by: NAHO <90870942+trueNAHO@users.noreply.github.com>

stylix/autoload.nix
@@ -42,7 +42,19 @@ builtins.concatLists (
         {
           key = file;
           _file = file;
-          imports = [ (module (args // extraArgs // { inherit mkTarget; })) ];
+          imports = [
+            (module (
+              args
+              // extraArgs
+              // {
+                inherit mkTarget;
+                config = lib.recursiveUpdate config {
+                  stylix = throw "stylix: unguareded `config.stylix` accessed while using mkTarget";
+                  lib.stylix.colors = throw "stylix: unguareded `config.lib.stylix.colors` accessed while using mkTarget";
+                };
+              }
+            ))
+          ];
         }
       else
         file

modules/bemenu/hm.nix
@@ -1,31 +1,28 @@
-{
-  mkTarget,
-  lib,
-  config,
-  ...
-}:
+{ mkTarget, lib, ... }:
 mkTarget {
   name = "bemenu";
   humanName = "bemenu";
 
-  extraOptions = {
-    fontSize = lib.mkOption {
-      description = ''
-        Font size used for bemenu.
-      '';
-      type = with lib.types; nullOr int;
-      default = config.stylix.fonts.sizes.popups;
-      defaultText = lib.literalExpression "config.stylix.fonts.sizes.popups";
-    }; # optional argument
+  extraOptions =
+    { fonts }:
+    {
+      fontSize = lib.mkOption {
+        description = ''
+          Font size used for bemenu.
+        '';
+        type = with lib.types; nullOr int;
+        default = fonts.sizes.popups;
+        defaultText = lib.literalExpression "config.stylix.fonts.sizes.popups";
+      }; # optional argument
 
-    alternate = lib.mkOption {
-      description = ''
-        Whether to use alternating colours.
-      '';
-      type = lib.types.bool;
-      default = false;
+      alternate = lib.mkOption {
+        description = ''
+          Whether to use alternating colours.
+        '';
+        type = lib.types.bool;
+        default = false;
+      };
     };
-  };
 
   configElements = [
     (

modules/hyprland/hm.nix
@@ -7,11 +7,16 @@
 mkTarget {
   name = "hyprland";
   humanName = "Hyprland";
-  extraOptions.hyprpaper.enable = config.lib.stylix.mkEnableTargetWith {
-    name = "Hyprpaper";
-    autoEnable = config.stylix.image != null;
-    autoEnableExpr = "config.stylix.image != null";
-  };
+  extraOptions =
+    { image }:
+    {
+      hyprpaper.enable = config.lib.stylix.mkEnableTargetWith {
+        name = "Hyprpaper";
+        autoEnable = image != null;
+        autoEnableExpr = "config.stylix.image != null";
+      };
+    };
+
   configElements = [
     (
       { colors }:

stylix/mk-target.nix
@@ -241,7 +241,9 @@ let
           c;
     in
     {
-      inherit imports;
+      imports = imports ++ [
+        { options.stylix.targets.${name} = mkConfig (lib.toFunction extraOptions); }
+      ];
 
       options.stylix.targets.${name}.enable =
         let
@@ -273,8 +275,5 @@ let
     };
 in
 {
-  imports = [
-    { options.stylix.targets.${name} = extraOptions; }
-    module
-  ];
+  imports = [ module ];
 }

nix-community/stylix

commit 46bedc273396c7590beffbd0d9aed6c3d8e47269

stylix: fix 'unguarded' typo
Fixes: dea0337e0bff ("stylix: restrict access to config while using mkTarget (#1368)")

stylix/autoload.nix
@@ -49,8 +49,8 @@ builtins.concatLists (
               // {
                 inherit mkTarget;
                 config = lib.recursiveUpdate config {
-                  stylix = throw "stylix: unguareded `config.stylix` accessed while using mkTarget";
-                  lib.stylix.colors = throw "stylix: unguareded `config.lib.stylix.colors` accessed while using mkTarget";
+                  stylix = throw "stylix: unguarded `config.stylix` accessed while using mkTarget";
+                  lib.stylix.colors = throw "stylix: unguarded `config.lib.stylix.colors` accessed while using mkTarget";
                 };
               }
             ))