f7eb6ef6e770953717bf41d8c36a966a3e5c122c
awwpotato <awwpotato@voidq.com>
2025-07-08 19:25:42 -0700

    stylix: add generated all-maintainers file

--- flake/dev/default.nix
+++ flake/dev/default.nix
@@ -1,5 +1,6 @@
 {
   imports = [
+    ./build-all-maintainers.nix
     ./dev-shell.nix
     ./nixpkgs-and-dev-nixpkgs-version-consistency.nix
     ./packages.nix
--- flake/propagated-packages.nix
+++ flake/propagated-packages.nix
@@ -15,10 +15,17 @@
   perSystem =
     { pkgs, system, ... }:
     lib.optionalAttrs (partitionStack == [ ]) {
-      packages = lib.mkIf pkgs.stdenv.hostPlatform.isLinux (
-        lib.mapAttrs (
-          name: _: config.partitions.dev.module.flake.packages.${system}.${name}
-        ) (import ../stylix/testbed/autoload.nix { inherit lib pkgs; })
-      );
+      packages = lib.mkMerge [
+        {
+          inherit (config.partitions.dev.module.flake.packages.${system})
+            build-all-maintainers
+            ;
+        }
+        (lib.mkIf pkgs.stdenv.hostPlatform.isLinux (
+          lib.mapAttrs (
+            name: _: config.partitions.dev.module.flake.packages.${system}.${name}
+          ) (import ../stylix/testbed/autoload.nix { inherit lib pkgs; })
+        ))
+      ];
     };
 }
