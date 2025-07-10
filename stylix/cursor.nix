b7fa50a66fe6023dd78dd46a4e9812dc4e94c9c3
awwpotato <awwpotato@voidq.com>
2025-07-09 19:37:57 -0700

    stylix: use enable for cursor instead of null

--- stylix/cursor.nix
+++ stylix/cursor.nix
@@ -1,5 +1,4 @@
 { lib, config, ... }:
-
 {
   options.stylix.cursor = lib.mkOption {
     description = ''
@@ -9,6 +8,16 @@
     type = lib.types.nullOr (
       lib.types.submodule {
         options = {
+          enable = lib.mkEnableOption "cursor theming with stylix" // {
+            default =
+              lib.versionOlder (config.system.stateVersion or config.home.stateVersion
+              ) "25.05"
+              && builtins.any (x: x != null) (builtins.attrValues config.stylix.cursor);
+            defaultText = lib.literalExpression ''
+              lib.versionOlder (config.system.stateVersion or config.home.stateVersion) "25.05"
+              && builtins.any (x: x != null) (builtins.attrValues config.stylix.cursor)
+            '';
+          };
           name = lib.mkOption {
             description = "The cursor name within the package.";
             type = lib.types.nullOr lib.types.str;
@@ -27,21 +36,44 @@
         };
       }
     );
-    default = null;
-  };
-  config.assertions =
-    let
-      inherit (config.stylix) cursor;
-    in
-    [
+    default = {
+      enable =
+        lib.versionOlder (config.system.stateVersion or config.home.stateVersion
+        ) "25.05"
+        && builtins.any (x: x != null) (builtins.attrValues config.stylix.cursor);
+      name = null;
+      package = null;
+      size = null;
+    };
+    defaultText = lib.literalExpression ''
       {
-        assertion =
-          cursor == null
-          || cursor.name != null && cursor.package != null && cursor.size != null;
-        message = ''
-          stylix: `stylix.cursor` is only partially defined. Set either none or
-          all of the `stylix.cursor` options.
-        '';
+        enable =
+          lib.versionOlder (config.system.stateVersion or config.home.stateVersion) "25.05"
+          && builtins.any (x: x != null) (builtins.attrValues config.stylix.cursor);
+        name = null;
+        package = null;
+        size = null;
       }
-    ];
+    '';
+  };
+  config = {
+    assertions =
+      let
+        inherit (config.stylix) cursor;
+      in
+      [
+        {
+          assertion =
+            (cursor == null && cursor.enable)
+            || cursor.name != null && cursor.package != null && cursor.size != null;
+          message = ''
+            stylix: `stylix.cursor` is only partially defined. Set either none or
+            all of the `stylix.cursor` options.
+          '';
+        }
+      ];
+    warnings =
+      lib.optional (config.stylix.cursor == null)
+        "stylix: setting `stylix.cursor` to null is deprecated, instead set `stylix.cursor.enable` to `false`.";
+  };
 }
