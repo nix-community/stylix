1330e2386e1d70fd1ef2538147bd9facba5c1f76
Louis Dalibard <louis.dalibard@gmail.com>
2025-07-16 14:45:37 +0200

    Update stylix/palette.nix
    Co-authored-by: NAHO <90870942+trueNAHO@users.noreply.github.com>

--- stylix/palette.nix
+++ stylix/palette.nix
@@ -131,7 +131,7 @@ in
                   --type scheme-$SCHEME \
                   image \
                   "$IMAGE" \
-                  > "$out"
+                  >"$out"
             '';
       };
 
