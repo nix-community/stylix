nix-community/stylix

commit 093087e969e2ecfe1175dbf3cbc5c8624fde1c1e

stylix: add imports to mkTarget (#1363)
Link: https://github.com/nix-community/stylix/pull/1363

Reviewed-by: NAHO <90870942+trueNAHO@users.noreply.github.com>

stylix/mk-target.nix
@@ -156,6 +156,7 @@
   extraOptions ? { },
   configElements ? [ ],
   generalConfig ? null,
+  imports ? [ ],
 }:
 let
   module =
@@ -208,6 +209,8 @@ let
           c;
     in
     {
+      inherit imports;
+
       options.stylix.targets.${name}.enable =
         config.lib.stylix.mkEnableTarget humanName autoEnable;
 

commit b37a46e11bc1f15ad59ec70d937b464c952b90fc

stylix/mk-target: document imports argument
Fixes: 093087e969e2 ("stylix: add imports to mkTarget (#1363)")

stylix/mk-target.nix
@@ -130,6 +130,9 @@
       lacks automatic safeguarding and should only be used for complex
       configurations where `configElements` is unsuitable.
 
+    `imports` (List)
+    : The `imports` option forwarded to the Nixpkgs module system.
+
   # Environment
 
   The function is provided alongside module arguments in any modules imported