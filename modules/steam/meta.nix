10c25234c872db0a671375cdcd295eee4ddf4072
bricked <git@bricked.dev>
2025-07-23 14:47:52 +0000

    steam: format meta
    Co-authored-by: NAHO <90870942+trueNAHO@users.noreply.github.com>

--- modules/steam/meta.nix
+++ modules/steam/meta.nix
@@ -1,6 +1,6 @@
 { lib, ... }:
 {
-  maintainers = [ lib.maintainers.bricked ];
   name = "Steam";
-  homepage = "https://store.steampowered.com/";
+  homepage = "https://store.steampowered.com";
+  maintainers = [ lib.maintainers.bricked ];
 }
