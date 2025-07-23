mightyiam/infra

commit 1021b7d7322c4a3eada82bc6e60c6bedf08cfce1

stylix: simplify API by renaming stylix.iconTheme option to stylix.icons

stylix/icons.nix
@@ -1,6 +1,20 @@
 { lib, ... }:
 {
-  options.stylix.iconTheme = {
+  imports = [
+    (lib.mkRenamedOptionModuleWith {
+      from = [
+        "stylix"
+        "iconTheme"
+      ];
+      sinceRelease = 2511;
+      to = [
+        "stylix"
+        "icons"
+      ];
+    })
+  ];
+
+  options.stylix.icons = {
     enable = lib.mkOption {
       description = "enable/disable icon theming.";
       type = lib.types.bool;

stylix/hm/icons.nix
@@ -1,6 +1,6 @@
 { config, lib, ... }:
 let
-  cfg = config.stylix.iconTheme;
+  cfg = config.stylix.icons;
   inherit (config.stylix) polarity;
 in
 {

modules/fuzzel/hm.nix
@@ -33,10 +33,10 @@ mkTarget {
       }
     )
     (
-      { polarity, iconTheme }:
+      { polarity, icons }:
       {
-        programs.fuzzel.settings.main."icon-theme" =
-          if (polarity == "dark") then iconTheme.dark else iconTheme.light;
+        programs.fuzzel.settings.main."icons" =
+          if (polarity == "dark") then icons.dark else icons.light;
       }
     )
   ];

modules/qt/hm.nix
@@ -33,11 +33,11 @@
 
   config = lib.mkIf (config.stylix.enable && config.stylix.targets.qt.enable) (
     let
-      iconTheme =
+      icons =
         if (config.stylix.polarity == "dark") then
-          config.stylix.iconTheme.dark
+          config.stylix.icons.dark
         else
-          config.stylix.iconTheme.light;
+          config.stylix.icons.light;
 
       recommendedStyles = {
         gnome = if config.stylix.polarity == "dark" then "adwaita-dark" else "adwaita";
@@ -91,8 +91,8 @@
             + lib.optionalString (config.qt.style ? name) ''
               style=${config.qt.style.name}
             ''
-            + lib.optionalString (iconTheme != null) ''
-              icon_theme=${iconTheme}
+            + lib.optionalString (icons != null) ''
+              icon_theme=${icons}
             '';
 
         in

stylix/hm/default.nix
@@ -9,11 +9,11 @@ in
 {
   imports = [
     ./cursor.nix
-    ./icon-theme.nix
+    ./icons.nix
     ./palette.nix
     ../cursor.nix
     ../fonts.nix
-    ../icon-theme.nix
+    ../icons.nix
     ../opacity.nix
     ../palette.nix
     ../pixel.nix

nix-community/stylix

commit 535ece649fbac40de7d76abcce43781bf011136b

stylix: apply stylix.iconTheme option rename to all sub-options
Fixes: 1021b7d7322c ("stylix: simplify API by renaming stylix.iconTheme option to stylix.icons")

stylix/icons.nix
@@ -5,11 +5,52 @@
       from = [
         "stylix"
         "iconTheme"
+        "dark"
       ];
       sinceRelease = 2511;
       to = [
         "stylix"
         "icons"
+        "dark"
+      ];
+    })
+    (lib.mkRenamedOptionModuleWith {
+      from = [
+        "stylix"
+        "iconTheme"
+        "enable"
+      ];
+      sinceRelease = 2511;
+      to = [
+        "stylix"
+        "icons"
+        "enable"
+      ];
+    })
+    (lib.mkRenamedOptionModuleWith {
+      from = [
+        "stylix"
+        "iconTheme"
+        "light"
+      ];
+      sinceRelease = 2511;
+      to = [
+        "stylix"
+        "icons"
+        "light"
+      ];
+    })
+    (lib.mkRenamedOptionModuleWith {
+      from = [
+        "stylix"
+        "iconTheme"
+        "package"
+      ];
+      sinceRelease = 2511;
+      to = [
+        "stylix"
+        "icons"
+        "package"
       ];
     })
   ];