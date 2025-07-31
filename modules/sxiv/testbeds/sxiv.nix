mightyiam/infra

commit 8b898ca041a3c134d46feea7e94ca136aa7d7de6

sxiv: avoid downloading custom file in testbed (#1641)
Fixes: 965865bc3603 ("sxiv: add testbed (#1190)")
Link: https://github.com/nix-community/stylix/pull/1641

Reviewed-by: Mateus Auler <mateus@auler.dev>

modules/sxiv/testbeds/sxiv.nix
@@ -4,18 +4,13 @@ let
   package = pkgs.nsxiv;
 in
 {
-  stylix.testbed.ui.command.text =
-    let
-      image = pkgs.fetchurl {
-        url = "https://raw.githubusercontent.com/NixOS/nixos-artwork/4ad062cee62116f6055e2876e9638e7bb399d219/logo/nixos.svg.png";
-        hash = "sha256-9+OfqfP5LmubdTcwBkS/AnOX4wZI2tKHLu5nhi43xcc=";
-      };
-    in
-    ''
-      # Xresources isn't loaded by default, so we force it
-      ${lib.getExe pkgs.xorg.xrdb} ~/.Xresources
-      ${lib.getExe package} ${image}
-    '';
+  stylix.testbed.ui.command.text = ''
+    # Xresources isn't loaded by default, so we force it
+    ${lib.getExe pkgs.xorg.xrdb} ~/.Xresources
+
+    ${lib.getExe package} \
+      "${pkgs.nixos-icons}/share/icons/hicolor/scalable/apps/nix-snowflake.svg";
+  '';
 
   home-manager.sharedModules = lib.singleton {
     home.packages = [ package ];

nix-community/stylix

commit ae619897132c2095be549e30bde7ef5ba23f5f8b

sxiv/testbed: remove trailing Bash semicolon
Fixes: 8b898ca041a3 ("sxiv: avoid downloading custom file in testbed (#1641)")

modules/sxiv/testbeds/sxiv.nix
@@ -8,7 +8,7 @@ in
     ${lib.getExe pkgs.xorg.xrdb} ~/.Xresources
 
     ${lib.getExe package} \
-      "${pkgs.nixos-icons}/share/icons/hicolor/scalable/apps/nix-snowflake.svg";
+      "${pkgs.nixos-icons}/share/icons/hicolor/scalable/apps/nix-snowflake.svg"
   '';
 
   home-manager.sharedModules = lib.singleton {