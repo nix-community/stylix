334394267791092b7dab54c7782d438defdcf95c
bricked <spider@bricked.dev>
2025-09-08 21:33:15 +0200

    steam: apply adwsteamgtk patch using overlay and remove adwaitaTheme option

--- modules/steam/hm.nix
+++ modules/steam/hm.nix
@@ -1,44 +1,17 @@
 {
-  lib,
   pkgs,
   mkTarget,
   ...
 }:
 
-let
-  adwsteamgtk = pkgs.adwsteamgtk.overridePythonAttrs {
-    # See https://github.com/Foldex/AdwSteamGtk/pull/95
-    src = pkgs.fetchFromGitHub {
-      owner = "Foldex";
-      repo = "AdwSteamGtk";
-      rev = "a003d50a6adab743356b5f2538db83bb78ea6e36";
-      hash = "sha256-zBlNKtV8VpElxeJfjXJudOTMpjmfZdzSEOCuu4FgJ0s=";
-    };
-  };
-in
 mkTarget {
   name = "steam";
   humanName = "Steam";
 
-  extraOptions = {
-    adwaitaTheme.enable = lib.mkOption {
-      type = lib.types.bool;
-      default = false;
-      example = true;
-      description = ''
-        Whether to enable the Adwaita theme for Steam.
-
-        > [!IMPORTANT]
-        > This option will install and configure the AdwSteamGtk application.
-        > The theme can be applied by running this application.
-      '';
-    };
-  };
-
   configElements =
-    { cfg, colors }:
-    lib.mkIf cfg.adwaitaTheme.enable {
-      home.packages = [ adwsteamgtk ];
+    { colors }:
+    {
+      home.packages = [ pkgs.adwsteamgtk ];
 
       dconf.settings."io/github/Foldex/AdwSteamGtk".prefs-install-custom-css = true;
 
--- modules/steam/meta.nix
+++ modules/steam/meta.nix
@@ -3,4 +3,10 @@
   name = "Steam";
   homepage = "https://store.steampowered.com";
   maintainers = [ lib.maintainers.bricked ];
+  description = ''
+    > [!IMPORTANT]
+
+    > You need to run the AdwSteamGTK application to apply this theme.
+    > Requires `programs.dconf` to be enabled in the NixOS configuration.
+  '';
 }
