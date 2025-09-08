6967dc706b340f03a8b2532857850366d66a3b89
bricked <spider@bricked.dev>
2025-09-08 21:33:15 +0200

    steam: apply adwsteamgtk patch using overlay and remove adwaitaTheme option

---
+++ modules/steam/overlay.nix
@@ -0,0 +1,18 @@
+{ config, lib, ... }:
+{
+  overlay =
+    _final: prev:
+    lib.optionalAttrs
+      (
+        config.stylix.enable
+        && config.stylix.targets ? steam
+        && config.stylix.targets.steam.enable
+      )
+      {
+        adwsteamgtk = prev.adwsteamgtk.overrideAttrs (oldAttrs: {
+          patches = (oldAttrs.patches or [ ]) ++ [
+            ./fix_custom_css_permissions.patch
+          ];
+        });
+      };
+}
