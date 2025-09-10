38a9ecbe1ae042668335baff4655779af766dda1
Ivy Pierlot <ivyp@outlook.com.au>
2025-09-10 17:00:39 +1000

    fixup! fixup! stylix/darwin: add module

--- modules/darwin/hm.nix
+++ modules/darwin/hm.nix
@@ -9,43 +9,43 @@
 mkTarget {
   name = "darwin";
   humanName = "Darwin";
-  extraOptions =
-    {
-      image,
-    }:
-    {
-      home.activation = {
-        stylixBackground = ''
-          run ${pkgs.callPackage ./desktoppr.nix { }}/bin/desktoppr all ${image}
-
-        '';
-      };
-    };
 
   configElements = [
-
+    (
+      {
+        image,
+        pkgs,
+      }:
+      {
+        home.activation = {
+          stylixBackground = ''
+            run ${pkgs.callPackage ../../stylix/darwin/desktoppr.nix { }}/bin/desktoppr all ${image}
+          '';
+        };
+      }
+    )
     (
       {
         colors,
       }:
-      let
-        value = pkgs.runCommand "color" { } ''
 
-                    full=$((16#${colors.base00}))
-                    red=$((($full >> 16) & 0xff))
-          red=$(echo "scale=10; $red.0 / 255.0 " | bc |awk '{printf "%f", $0}')
-          green=$((($full >> 8) & 0xff))
-          green=$(echo "scale=10; $green.0 / 255.0  " | bc | awk '{printf "%f", $0}')
-          blue=$(($full & 0xff))
-          blue=$(echo "scale=10; $blue.0 / 255.0 " | bc | awk '{printf "%f", $0}')
-          echo "$red $green $blue 1.0" > $out
-
-        '';
-
-      in
       {
 
-        targets.darwin.defaults."Apple Global Domain".AppleIconAppearanceCustomTintColor = value;
+        targets.darwin.defaults."Apple Global Domain" = {
+          AppleIconAppearanceCustomTintColor = builtins.readFile "${
+            pkgs.runCommand "color" { } ''
+              full=$((16#${colors.base00}))
+              red=$((($full >> 16) & 0xff))
+              red=$(echo "scale=10; $red.0 / 255.0 " | bc |awk '{printf "%f", $0}')
+              green=$((($full >> 8) & 0xff))
+              green=$(echo "scale=10; $green.0 / 255.0  " | bc | awk '{printf "%f", $0}')
+              blue=$(($full & 0xff))
+              blue=$(echo "scale=10; $blue.0 / 255.0 " | bc | awk '{printf "%f", $0}')
+              echo "$red $green $blue 1.0" > $out
+            ''
+
+          }";
+        };
       }
     )
   ];
