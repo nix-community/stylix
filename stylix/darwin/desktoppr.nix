3fc8fc8534b0f05fd2e955bbe70035118e8acf6c
Ivy Pierlot <ivyp@outlook.com.au>
2025-09-10 17:00:39 +1000

    fixup! fixup! stylix/darwin: add module

--- stylix/darwin/desktoppr.nix
+++ stylix/darwin/desktoppr.nix
@@ -5,9 +5,9 @@
   unzip,
 }:
 stdenv.mkDerivation {
-  name = "hln";
+  name = "desktoppr";
   version = "0.5-218";
-  pname = "hln";
+  pname = "desktoppr";
   src = fetchurl {
     url = "https://github.com/scriptingosx/desktoppr/releases/download/v0.5/desktoppr-0.5-218.zip";
     hash = "sha256-Oa9gAQjOaJHYyT5JBUiFCxL1sQP1dqlFBm+GdmLHNNM=";
