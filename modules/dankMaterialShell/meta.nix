02acd2d3ff06f838c7d9dfbbcdc9ac3ed6dc2872
khas-amir <dlordproudd@gmail.com>
2025-10-05 01:48:05 +0300

    DankMaterialShell: init target

---
+++ modules/dankMaterialShell/meta.nix
@@ -0,0 +1,7 @@
+{ lib, ... }:
+{
+  name = "dankMaterialShell";
+  homepage = "https://github.com/AvengeMedia/DankMaterialShell";
+  maintainers = [ lib.maintainers.khas-amir ];
+  description = ''In the settings, click Theme & Colors, then Custom, and select the path to the Stylix-generated color scheme ` ~/.config/DankMaterialShell/stylix-colors.json`'';
+}
