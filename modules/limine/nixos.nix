d52ee20dfe42a86ab44f521f5db95e41356a708d
jamie <git@skilet.ro>
2025-10-31 09:14:58 +0000

    limine: add wallpaper

--- modules/limine/nixos.nix
+++ modules/limine/nixos.nix
@@ -1,21 +1,43 @@
-{ mkTarget, ... }:
+{
+  mkTarget,
+  config,
+  lib,
+  ...
+}:
 mkTarget {
   name = "Limine";
   humanName = "Limine";
 
-  configElements =
-    { colors }:
-    {
-      boot.loader.limine.style = with colors; {
-        graphicalTerminal = {
-          palette = "${base05};${base08};${base0B};${base0A};${base0D};${base0E};${base0C};${base00}";
-          brightPalette = "${base00};${base08};${base0B};${base0A};${base0D};${base0E};${base0C};${base05}";
-          background = base00;
-          foreground = base05;
-          brightBackground = base05;
-          brightForeground = base0A;
-        };
-        wallpapers = [ ];
-      };
-    };
+  extraOptions.useWallpaper = config.lib.stylix.mkEnableWallpaper "Limine" true;
+
+  configElements = [
+    (
+      { colors }:
+      {
+        boot.loader.limine =
+          with colors;
+          lib.mkIf (config.stylix.enable && cfg.enable) {
+            style = {
+              graphicalTerminal = {
+                palette = "${base05};${base08};${base0B};${base0A};${base0D};${base0E};${base0C};${base00}";
+                brightPalette = "${base00};${base08};${base0B};${base0A};${base0D};${base0E};${base0C};${base05}";
+                background = base00;
+                foreground = base05;
+                brightBackground = base05;
+                brightForeground = base0A;
+              };
+            };
+          };
+      }
+    )
+    (
+      { cfg, image }:
+      {
+
+        boot.loader.limine.style.wallpapers = lib.mkIf cfg.useWallpaper [
+          image
+        ];
+      }
+    )
+  ];
 }
