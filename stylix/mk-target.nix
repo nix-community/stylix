mightyiam/infra

commit dea0337e0bffeeeb941ca6caffb44e966b13a97b

stylix: restrict access to config while using mkTarget (#1368)
Link: https://github.com/nix-community/stylix/pull/1368

Reviewed-by: Flameopathic <64027365+Flameopathic@users.noreply.github.com>
Reviewed-by: Matt Sturgeon <matt@sturgeon.me.uk>
Reviewed-by: NAHO <90870942+trueNAHO@users.noreply.github.com>

stylix/mk-target.nix
@@ -241,7 +241,9 @@ let
           c;
     in
     {
-      inherit imports;
+      imports = imports ++ [
+        { options.stylix.targets.${name} = mkConfig (lib.toFunction extraOptions); }
+      ];
 
       options.stylix.targets.${name}.enable =
         let
@@ -273,8 +275,5 @@ let
     };
 in
 {
-  imports = [
-    { options.stylix.targets.${name} = extraOptions; }
-    module
-  ];
+  imports = [ module ];
 }

modules/bemenu/hm.nix
@@ -1,31 +1,28 @@
-{
-  mkTarget,
-  lib,
-  config,
-  ...
-}:
+{ mkTarget, lib, ... }:
 mkTarget {
   name = "bemenu";
   humanName = "bemenu";
 
-  extraOptions = {
-    fontSize = lib.mkOption {
-      description = ''
-        Font size used for bemenu.
-      '';
-      type = with lib.types; nullOr int;
-      default = config.stylix.fonts.sizes.popups;
-      defaultText = lib.literalExpression "config.stylix.fonts.sizes.popups";
-    }; # optional argument
+  extraOptions =
+    { fonts }:
+    {
+      fontSize = lib.mkOption {
+        description = ''
+          Font size used for bemenu.
+        '';
+        type = with lib.types; nullOr int;
+        default = fonts.sizes.popups;
+        defaultText = lib.literalExpression "config.stylix.fonts.sizes.popups";
+      }; # optional argument
 
-    alternate = lib.mkOption {
-      description = ''
-        Whether to use alternating colours.
-      '';
-      type = lib.types.bool;
-      default = false;
+      alternate = lib.mkOption {
+        description = ''
+          Whether to use alternating colours.
+        '';
+        type = lib.types.bool;
+        default = false;
+      };
     };
-  };
 
   configElements = [
     (

modules/hyprland/hm.nix
@@ -7,11 +7,16 @@
 mkTarget {
   name = "hyprland";
   humanName = "Hyprland";
-  extraOptions.hyprpaper.enable = config.lib.stylix.mkEnableTargetWith {
-    name = "Hyprpaper";
-    autoEnable = config.stylix.image != null;
-    autoEnableExpr = "config.stylix.image != null";
-  };
+  extraOptions =
+    { image }:
+    {
+      hyprpaper.enable = config.lib.stylix.mkEnableTargetWith {
+        name = "Hyprpaper";
+        autoEnable = image != null;
+        autoEnableExpr = "config.stylix.image != null";
+      };
+    };
+
   configElements = [
     (
       { colors }:

stylix/autoload.nix
@@ -42,7 +42,19 @@ builtins.concatLists (
         {
           key = file;
           _file = file;
-          imports = [ (module (args // extraArgs // { inherit mkTarget; })) ];
+          imports = [
+            (module (
+              args
+              // extraArgs
+              // {
+                inherit mkTarget;
+                config = lib.recursiveUpdate config {
+                  stylix = throw "stylix: unguareded `config.stylix` accessed while using mkTarget";
+                  lib.stylix.colors = throw "stylix: unguareded `config.lib.stylix.colors` accessed while using mkTarget";
+                };
+              }
+            ))
+          ];
         }
       else
         file

nix-community/stylix

commit 81c5ea40b0caab5e85c875863aefa00b0a21a038

stylix/mk-target: rename mkConfig function to callModule
Rename the mkConfig function to callModule to generalize the name beyond
the specific config and options module arguments.

Fixes: dea0337e0bff ("stylix: restrict access to config while using mkTarget (#1368)")

stylix/mk-target.nix
@@ -217,7 +217,7 @@ let
         );
 
       # Call the configuration function with its required Stylix arguments.
-      mkConfig = fn: fn (getStylixAttrs fn);
+      callModule = fn: fn (getStylixAttrs fn);
 
       # Safeguard configuration functions when any of their arguments is
       # disabled.
@@ -232,13 +232,13 @@ let
               (builtins.all (attr: attr.enable or (attr != null)))
             ];
           in
-          lib.mkIf allAttrsEnabled (mkConfig c)
+          lib.mkIf allAttrsEnabled (callModule c)
         else
           c;
     in
     {
       imports = imports ++ [
-        { options.stylix.targets.${name} = mkConfig (lib.toFunction extraOptions); }
+        { options.stylix.targets.${name} = callModule (lib.toFunction extraOptions); }
       ];
 
       options.stylix.targets.${name}.enable =
@@ -258,7 +258,7 @@ let
       config = lib.mkIf (config.stylix.enable && cfg.enable) (
         lib.mkMerge (
           lib.optional (generalConfig != null) (
-            mkConfig (
+            callModule (
               if builtins.isPath generalConfig then import generalConfig else generalConfig
             )
           )