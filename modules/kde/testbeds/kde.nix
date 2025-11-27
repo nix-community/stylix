82cbe4c4f82d1b14829b9746eddd1cf262f7290b
AndreBClark <andre@cosmicdivision.dev>
2025-11-26 21:47:34 -0800

    kde: move new options to sharedModules in testbed
    added custom widgetStyle and applicationStyles to testbed settings in order to test that the non default values are applied.
    Do note that having stylix.targets.kde settings just in config resulted in error  stylix.targets.kde option "does not exist"

--- modules/kde/testbeds/kde.nix
+++ modules/kde/testbeds/kde.nix
@@ -6,18 +6,20 @@
 {
   config = {
     stylix.testbed.ui.graphicalEnvironment = "kde";
-    services.displayManager.autoLogin.enable = lib.mkForce false;
-    home-manager.sharedModules = lib.singleton {
-      home.packages = [
-        pkgs.darkly
-        pkgs.darkly-qt5
-        pkgs.utterly-round-plasma-style
-      ];
-      stylix.targets.kde = {
-        widgetStyle = "Darkly";
-        applicationStyle = "Utterly-Round";
-      };
-    };
 
+    services.displayManager.autoLogin.enable = lib.mkForce false;
+
+    home-manager.sharedModules = lib.singleton {
+      stylix.targets.kde = {
+        enable = true;
+        applicationStyle = "Utterly-Round";
+        widgetStyle = "Darkly";
+      };
+      home.packages = with pkgs; [
+        darkly
+        darkly-qt5
+        utterly-round-plasma-style
+      ];
+    };
   };
 }
