mightyiam/infra

commit b9b050183938b7cda58d2e7060ccd2cb3dba8ddf

gdu: init (#1995)
Link: https://github.com/nix-community/stylix/pull/1995

Reviewed-by: NAHO <90870942+trueNAHO@users.noreply.github.com>

modules/gdu/hm.nix
@@ -1,0 +1,32 @@
+{
+  mkTarget,
+  ...
+}:
+mkTarget {
+  name = "go DiskUsage()";
+  humanName = "go DiskUsage()";
+
+  configElements =
+    { colors }:
+    {
+      xdg.configFile."gdu/gdu.yaml" = {
+        enable = true;
+        text = with colors.withHashtag; ''
+          style:
+            selected-row:
+              text-color: "${base05}"
+              background-color: "${base00}"
+            result-row:
+              number-color: "${base06}"
+              directory-color: "${base02}"
+            footer:
+              text-color: "${base05}"
+              background-color: "${base00}"
+              number-color: "${base06}"
+            header:
+              text-color: "${base05}"
+              background-color: "${base00}"
+        '';
+      };
+    };
+}

modules/gdu/meta.nix
@@ -1,0 +1,6 @@
+{ lib, ... }:
+{
+  name = "gdu";
+  homepage = "https://github.com/dundee/gdu";
+  maintainers = [ lib.maintainers.omega-800 ];
+}

generated/all-maintainers.nix
@@ -187,6 +187,12 @@
     ];
     name = "Viktor Titov";
   };
+  omega-800 = {
+    email = "gshevoroshkin@gmail.com";
+    github = "omega-800";
+    githubId = 50942480;
+    name = "Georgiy Shevoroshkin";
+  };
   osipog = {
     email = "osibluber@protonmail.com";
     github = "osipog";

modules/gdu/testbeds/gdu.nix
@@ -1,0 +1,11 @@
+{ lib, pkgs, ... }:
+{
+  stylix.testbed.ui.command = {
+    text = "gdu";
+    useTerminal = true;
+  };
+
+  home-manager.sharedModules = lib.singleton {
+    home.packages = [ pkgs.gdu ];
+  };
+}

stylix/maintainers.nix
@@ -53,6 +53,12 @@
     github = "michaelgoldenn";
     githubId = 95949544;
   };
+  omega-800 = {
+    email = "gshevoroshkin@gmail.com";
+    name = "Georgiy Shevoroshkin";
+    github = "omega-800";
+    githubId = 50942480;
+  };
   osipog = {
     email = "osibluber@protonmail.com";
     name = "Osi Bluber";

nix-community/stylix

commit 54484e841560b2aa63c4a7cff8b748b281354cfb

gdu: properly rename module and option names
Fixes: b9b050183938 ("gdu: init (#1995)")

modules/gdu/hm.nix
@@ -3,7 +3,7 @@
   ...
 }:
 mkTarget {
-  name = "go DiskUsage()";
+  name = "gdu";
   humanName = "go DiskUsage()";
 
   configElements =

modules/gdu/meta.nix
@@ -1,6 +1,6 @@
 { lib, ... }:
 {
-  name = "gdu";
+  name = "go DiskUsage()";
   homepage = "https://github.com/dundee/gdu";
   maintainers = [ lib.maintainers.omega-800 ];
 }