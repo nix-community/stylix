6d5708ee0c229eaaadf502a1198feaeb23280843
0xda157 <da157@voidq.com>
2025-12-02 06:40:02 -1000

    flake: use 25.11 branch of dependencies (#2026)
    Link: https://github.com/nix-community/stylix/pull/2026
    
    Reviewed-by: NAHO <90870942+trueNAHO@users.noreply.github.com>

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "Theming framework for NixOS, Home Manager, nix-darwin, and Nix-on-Droid";
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.11";
 
     flake-parts = {
       url = "github:hercules-ci/flake-parts";
