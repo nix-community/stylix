c49a504b21bce7171fb37dfc75df1283ac3e4f4c
0xda157 <da157@voidq.com>
2025-12-03 13:46:54 -0800

    flake: add remaining drvs to ci.buildbot
    Closes: https://github.com/nix-community/stylix/issues/2056

--- flake/dev/treefmt.nix
+++ flake/dev/treefmt.nix
@@ -2,41 +2,47 @@
 {
   imports = [ inputs.treefmt-nix.flakeModule ];
 
-  perSystem.treefmt = {
-    projectRootFile = "flake.nix";
+  perSystem =
+    { config, ... }:
+    {
+      ci.buildbot = { inherit (config.checks) treefmt; };
 
-    programs = {
-      # keep-sorted start block=yes newline_separated=no
-      biome = {
-        enable = true;
-        settings.formatter = {
-          indentStyle = "space";
-          indentWidth = 2;
-          lineWidth = 80;
+      treefmt = {
+        projectRootFile = "flake.nix";
+
+        programs = {
+          # keep-sorted start block=yes newline_separated=no
+          biome = {
+            enable = true;
+            settings.formatter = {
+              indentStyle = "space";
+              indentWidth = 2;
+              lineWidth = 80;
+            };
+            includes = [
+              "*.css"
+              "*.js"
+              "*.json"
+            ];
+            excludes = [
+              # Contains custom syntax that biome can't handle
+              "modules/swaync/base.css"
+            ];
+          };
+          keep-sorted.enable = true;
+          nixfmt = {
+            enable = true;
+            width = 80;
+            strict = true;
+          };
+          ruff-format = {
+            enable = true;
+            lineLength = 80;
+          };
+          shfmt.enable = true;
+          stylish-haskell.enable = true;
+          # keep-sorted end
         };
-        includes = [
-          "*.css"
-          "*.js"
-          "*.json"
-        ];
-        excludes = [
-          # Contains custom syntax that biome can't handle
-          "modules/swaync/base.css"
-        ];
       };
-      keep-sorted.enable = true;
-      nixfmt = {
-        enable = true;
-        width = 80;
-        strict = true;
-      };
-      ruff-format = {
-        enable = true;
-        lineLength = 80;
-      };
-      shfmt.enable = true;
-      stylish-haskell.enable = true;
-      # keep-sorted end
     };
-  };
 }
