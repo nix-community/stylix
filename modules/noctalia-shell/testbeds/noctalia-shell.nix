bdb25de7dd65a90825c05eca67e0b8dc4a050f0c
wardxela <wardxela@gmail.com>
2025-12-08 16:25:05 +0300

    noctalia-shell: add testbeds

---
+++ modules/noctalia-shell/testbeds/noctalia-shell.nix
@@ -0,0 +1,9 @@
+{ lib, ... }:
+{
+  stylix.testbed.ui = {
+    graphicalEnvironment = "hyprland";
+    command.text = "noctalia-shell";
+  };
+
+  home-manager.sharedModules = lib.singleton { programs.noctalia-shell.enable = true; };
+}
