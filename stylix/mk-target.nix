mightyiam/infra

commit 6153df31ce4f68f5709755994cc49c48d737e98c

stylix/mk-target: normalize options argument identically to config
Normalize the options argument identically to config to provide a
coherent and extensible options interface.

stylix/mk-target.nix
@@ -111,9 +111,9 @@
     `imports` (List)
     : The `imports` option forwarded to the Nixpkgs module system.
 
-    `options` (Attribute set)
-    : Additional options to be added in the `stylix.targets.${name}` namespace
-      along the `stylix.targets.${name}.enable` option.
+    `options` (List or attribute set or function or path)
+    : Additional options to be added in the `stylix.targets.${name}` namespace,
+      normalized identically to `config`.
 
       For example, an extension guard used in the configuration can be declared
       as follows:
@@ -181,7 +181,7 @@ in
   humanName ? humanName',
   imports ? [ ],
   name ? name',
-  options ? { },
+  options ? [ ],
   unconditionalConfig ? { },
 }@args:
 let
@@ -276,6 +276,7 @@ let
         )) (lib.toList config);
 
       normalizedConfig = normalizeConfig mkTargetConfig;
+      normalizedOptions = normalizeConfig options;
     in
     {
       imports =
@@ -289,7 +290,7 @@ let
                   )
                   builtins.attrNames
                   (lib.remove "cfg")
-                ]) (normalizedConfig ++ normalizeConfig options)
+                ]) (normalizedConfig ++ normalizedOptions)
               ))
               (
                 argument:
@@ -321,7 +322,9 @@ let
               );
         }
         ++ imports
-        ++ lib.singleton { options.stylix.targets.${name} = callModule false options; };
+        ++ map (option: {
+          options.stylix.targets.${name} = callModule false option;
+        }) normalizedOptions;
 
       options.stylix.targets.${name}.enable =
         let

nix-community/stylix

commit b150527e8413e67c2b20b9092694c02d592f03a1

stylix/mk-target: rename normalizeConfig function to normalize
Rename the normalizeConfig function to normalize to generalize the name
beyond the specific config and options module arguments, following
commit 1272e6858e29 ("stylix/mk-target: rename mkConfig function to
callModule").

Fixes: 6153df31ce4f ("stylix/mk-target: normalize options argument identically to config")

stylix/mk-target.nix
@@ -268,15 +268,15 @@ let
                 )
           );
 
-      normalizeConfig =
+      normalize =
         config:
         map (lib.fix (
           self: config':
           if builtins.isPath config' then self (import config') else config'
         )) (lib.toList config);
 
-      normalizedConfig = normalizeConfig mkTargetConfig;
-      normalizedOptions = normalizeConfig options;
+      normalizedConfig = normalize mkTargetConfig;
+      normalizedOptions = normalize options;
     in
     {
       imports =