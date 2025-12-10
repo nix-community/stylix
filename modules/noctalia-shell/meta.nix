4424f01b8a06d70045a5522dd0c54ecb7ad35beb
wardxela <wardxela@gmail.com>
2025-12-10 15:36:14 +0300

    noctalia-shell: add important info in description

--- modules/noctalia-shell/meta.nix
+++ modules/noctalia-shell/meta.nix
@@ -3,4 +3,16 @@
   name = "Noctalia shell";
   homepage = "https://docs.noctalia.dev";
   maintainers = [ lib.maintainers.rwxae ];
+  description = ''
+    > [!IMPORTANT]
+    >
+    > If you previously configured Noctalia using the GUI, enabling this target
+    > **will override** your existing settings.
+    >
+    > You must migrate your current settings from `~/.config/noctalia/settings.json`
+    > to Nix using the `programs.noctalia-shell.settings` option.
+    >
+    > See the [official documentation](https://docs.noctalia.dev/getting-started/nixos/#noctalia-settings)
+    > for more information.
+  '';
 }
