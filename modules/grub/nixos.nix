mightyiam/infra

commit 3499e3ec704b00d2155e1be72bb8f34081e471e4

treewide: properly quote stylix.image when used as a shell argument
Closes: https://github.com/nix-community/stylix/issues/1720
Fixes: 218d4424b063 ("treewide: remove redundant stylix.image Nix store copies (#1659)")

modules/grub/nixos.nix
@@ -134,7 +134,11 @@ in
                 cfg.useWallpaper
               # Make sure the background image is .png by asking to convert it
               then
-                "${lib.getExe' pkgs.imagemagick "convert"} ${config.stylix.image} png32:$out/background.png"
+                ''
+                  ${lib.getExe' pkgs.imagemagick "convert"} \
+                    ${lib.escapeShellArg config.stylix.image} \
+                    "png32:$out/background.png"
+                ''
               else
                 "cp ${pixel "base00"} $out/background.png"
             }

stylix/palette.nix
@@ -83,7 +83,7 @@ in
         default = pkgs.runCommand "palette.json" { } ''
           ${lib.getExe cfg.paletteGenerator} \
             "${cfg.polarity}" \
-            ${cfg.image} \
+            ${lib.escapeShellArg cfg.image} \
             "$out"
         '';
       };

stylix/testbed/images.nix
@@ -9,7 +9,9 @@
   };
 
   light = fetchurl {
-    name = "three-bicycles.jpg";
+    # Includes a space to make sure the path is properly quoted via
+    # `lib.escapeShellArg` when used as a shell argument.
+    name = "three bicycles.jpg";
     url = "https://unsplash.com/photos/hwLAI5lRhdM/download?ixid=M3wxMjA3fDB8MXxhbGx8fHx8fHx8fHwxNzE2MzYxNDcwfA&force=true";
     hash = "sha256-S0MumuBGJulUekoGI2oZfUa/50Jw0ZzkqDDu1nRkFUA=";
   };

nix-community/stylix

commit c4ae2ea8d5fac9a8ca091c1e920e963472121ad6

treewide: remove redundant stylix.image escaping and string coercion
Remove redundant shell escaping and string coercion as stylix.image is
guaranteed to be a stringified store path with a valid string context
and no special characters.

Fixes: 3499e3ec704b ("treewide: properly quote stylix.image when used as a shell argument")

modules/grub/nixos.nix
@@ -136,7 +136,7 @@ in
               then
                 ''
                   ${lib.getExe' pkgs.imagemagick "convert"} \
-                    ${lib.escapeShellArg config.stylix.image} \
+                    ${config.stylix.image} \
                     "png32:$out/background.png"
                 ''
               else

modules/hyprlock/hm.nix
@@ -11,9 +11,7 @@ mkTarget {
     (
       { cfg, image }:
       {
-        programs.hyprlock.settings.background.path = lib.mkIf cfg.useWallpaper "${
-          image
-        }";
+        programs.hyprlock.settings.background.path = lib.mkIf cfg.useWallpaper image;
       }
     )
     (

modules/hyprpaper/hm.nix
@@ -5,7 +5,7 @@ mkTarget {
     {
       services.hyprpaper.settings.wallpaper = lib.singleton {
         monitor = "";
-        path = toString image;
+        path = image;
       };
     };
 }