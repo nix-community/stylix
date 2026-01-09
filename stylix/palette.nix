nix-community/stylix

commit 838df8b8ad7d993d4de4af144f57bca0d5d1329a

stylix: improve `stylix.image` type (#1414)
Previously:

- Out-of-store paths were not necessarily copied to store.

- Derivations were needlessly coerced to strings.

- `package` and `path` were both used, despite `path` being a superset
  of `package`.

Link: https://github.com/nix-community/stylix/pull/1414

Reviewed-by: awwpotato <awwpotato@voidq.com>
Reviewed-by: NAHO <90870942+trueNAHO@users.noreply.github.com>

stylix/palette.nix
@@ -29,7 +29,10 @@ in
     };
 
     image = lib.mkOption {
-      type = with lib.types; nullOr (coercedTo package toString path);
+      type = lib.types.nullOr lib.types.path;
+      # Ensure the path is copied to the store
+      apply =
+        value: if value == null || lib.isDerivation value then value else "${value}";
       description = ''
         Wallpaper image.
 

commit b2fa5a9fa1d2d2975099ed9d8ff94295db4457b5

stylix/palette: coerce derivations to store paths
Coerce derivations to store paths to ensure non-null values are
stringified store paths with valid string contexts.

Fixes: 838df8b8ad7d ("stylix: improve `stylix.image` type (#1414)")

Co-authored-by: Matt Sturgeon <matt@sturgeon.me.uk>

stylix/palette.nix
@@ -28,10 +28,8 @@ in
     };
 
     image = lib.mkOption {
-      type = lib.types.nullOr lib.types.path;
       # Ensure the path is copied to the store
-      apply =
-        value: if value == null || lib.isDerivation value then value else "${value}";
+      type = with lib.types; nullOr (coercedTo path (src: "${src}") pathInStore);
       description = ''
         Wallpaper image.
 