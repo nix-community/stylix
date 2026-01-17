6016953971610b8f261688563401452233dac8b2
admin <17591696+sand4rt@users.noreply.github.com>
2026-01-17 17:35:18 +0100

    chore: spread config across configElements

--- modules/swayimg/hm.nix
+++ modules/swayimg/hm.nix
@@ -1,46 +1,58 @@
-{
-  mkTarget,
-  lib,
-  config,
-  ...
-}:
+{ mkTarget, lib, ... }:
 mkTarget {
   name = "swayimg";
   humanName = "Swayimg";
 
-  configElements =
-    { colors, opacity }:
-    with colors.withHashtag;
-    let
-      swayimgOpacity = lib.toHexString (
-        builtins.floor (opacity.applications * 100 + 0.5) * 255 / 100
-      );
-    in
-    {
-      programs.swayimg.settings = {
-        viewer = {
-          window = base00 + swayimgOpacity;
-        };
-        slideshow = {
-          transparency = "#000000ff";
-        };
-        gallery = {
-          window = base00 + swayimgOpacity;
-          background = "#00000000";
-          select = base0D;
-          border = base0D;
-          shadow = "#00000000";
-        };
-        font =
-          with config.stylix.fonts.monospace;
-          with config.stylix.fonts.sizes;
-          {
-            inherit name;
-            size = applications;
+  configElements = [
+    (
+      { cfg, colors }:
+      with colors.withHashtag;
+      {
+        programs.swayimg.settings = {
+          viewer = {
+            window = base00 + cfg.opacity;
+          };
+          slideshow = {
+            transparency = "#000000ff";
+          };
+          gallery = {
+            window = base00 + cfg.opacity;
+            background = "#00000000";
+            select = base0D;
+            border = base0D;
+            shadow = "#00000000";
+          };
+          font = {
             color = base05;
             shadow = "#000000d0";
             background = "#00000000";
           };
-      };
-    };
+        };
+      }
+    )
+
+    (
+      { cfg, opacity }:
+      let
+        swayimgOpacity = lib.toHexString (
+          builtins.floor (opacity.applications * 100 + 0.5) * 255 / 100
+        );
+      in
+      {
+        cfg.opacity = swayimgOpacity;
+      }
+    )
+
+    (
+      { fonts }:
+      with fonts.monospace;
+      with fonts.sizes;
+      {
+        programs.swayimg.settings.font = {
+          inherit name;
+          size = applications;
+        };
+      }
+    )
+  ];
 }
