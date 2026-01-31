3dc4f3e4657b55b84843d7cf92f7be3082beea08
jamie <git@skilet.ro>
2026-01-31 18:58:34 +0000

    dank-material-shell: remove hacky 'light' theme support
    There isn't a nice way to make it look good with the limited colour
    palette. Users are advised to just set a light theme in Stylix if they
    want a light theme.

--- modules/dank-material-shell/hm.nix
+++ modules/dank-material-shell/hm.nix
@@ -10,57 +10,36 @@ mkTarget {
     (
       { colors }:
       let
-        colorTheme = with colors.withHashtag; {
-          dark = {
-            name = "Stylix generatated dark theme";
-            primary = base0D;
-            primaryText = base00;
-            primaryContainer = base0C;
-            secondary = base0E;
-            surface = base01;
-            surfaceText = base05;
-            surfaceVariant = base02;
-            surfaceVariantText = base04;
-            surfaceTint = base0D;
-            background = base00;
-            backgroundText = base05;
-            outline = base03;
-            surfaceContainer = base01;
-            surfaceContainerHigh = base02;
-            surfaceContainerHighest = base03;
-            error = base08;
-            warning = base0A;
-            info = base0C;
-          };
-
-          light = {
-            name = "Stylix generatated light theme";
-            primary = base0D;
-            primaryText = base07;
-            primaryContainer = base0C;
-            secondary = base0E;
-            surface = base06;
-            surfaceText = base01;
-            surfaceVariant = base07;
-            surfaceVariantText = base02;
-            surfaceTint = base0D;
-            background = base07;
-            backgroundText = base00;
-            outline = base04;
-            surfaceContainer = base06;
-            surfaceContainerHigh = base05;
-            surfaceContainerHighest = base04;
-            error = base08;
-            warning = base0A;
-            info = base0C;
-          };
+        theme = with colors.withHashtag; {
+          name = "Stylix";
+          primary = base0D;
+          primaryText = base00;
+          primaryContainer = base0C;
+          secondary = base0E;
+          surface = base01;
+          surfaceText = base05;
+          surfaceVariant = base02;
+          surfaceVariantText = base04;
+          surfaceTint = base0D;
+          background = base00;
+          backgroundText = base05;
+          outline = base03;
+          surfaceContainer = base01;
+          surfaceContainerHigh = base02;
+          surfaceContainerHighest = base03;
+          error = base08;
+          warning = base0A;
+          info = base0C;
         };
       in
       {
         programs.dank-material-shell.settings = {
           currentThemeName = "custom";
           customThemeFile = pkgs.writeText "dankMaterialShell-stylix-color-theme.json" (
-            builtins.toJSON colorTheme
+            builtins.toJSON {
+              dark = theme;
+              light = theme;
+            }
           );
         };
       }
