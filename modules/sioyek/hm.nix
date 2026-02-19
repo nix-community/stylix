mightyiam/infra

commit ec4d1ce9da22da964cef8a77795dc32011ebbb55

sioyek: init (#2199)
Closes: https://github.com/nix-community/stylix/issues/2148
Link: https://github.com/nix-community/stylix/pull/2199

Reviewed-by: NAHO <90870942+trueNAHO@users.noreply.github.com>

modules/sioyek/hm.nix
@@ -1,0 +1,31 @@
+{ mkTarget, lib, ... }:
+mkTarget {
+  config =
+    { colors }:
+    {
+      programs.sioyek.config = with colors.withHashtag; {
+        startup_commands = "toggle_custom_color";
+        background_color = base00;
+        text_highlight_color = base0A;
+        visual_mark_color = base02;
+        search_highlight_color = base0A;
+        link_highlight_color = base0D;
+        synctex_highlight_color = base0B;
+        highlight_color_a = base0A;
+        highlight_color_b = base0B;
+        highlight_color_c = base0D;
+        highlight_color_d = base08;
+        highlight_color_e = base0E;
+        highlight_color_f = base09;
+        highlight_color_g = base0A;
+        custom_background_color = base00;
+        custom_text_color = base05;
+        ui_text_color = base05;
+        ui_background_color = base01;
+        ui_selected_text_color = base05;
+        ui_selected_background_color = base02;
+        status_bar_color = base01;
+        status_bar_text_color = base05;
+      };
+    };
+}

generated/all-maintainers.nix
@@ -113,6 +113,11 @@
     matrix = "@danth:danth.me";
     name = "Daniel Thwaites";
   };
+  dastarruer = {
+    github = "dastarruer";
+    githubId = 173855353;
+    name = "dastarruer";
+  };
   gideonwolfe = {
     email = "wolfegideon@gmail.com";
     github = "gideonwolfe";

modules/sioyek/meta.nix
@@ -1,0 +1,6 @@
+{ lib, ... }:
+{
+  name = "Sioyek";
+  homepage = "https://sioyek.info";
+  maintainers = [ lib.maintainers.dastarruer ];
+}

modules/sioyek/testbeds/sioyek.nix
@@ -1,0 +1,17 @@
+{ lib, pkgs, ... }:
+let
+  package = pkgs.sioyek;
+in
+{
+  stylix.testbed.ui.application = {
+    name = "sioyek";
+    inherit package;
+  };
+
+  home-manager.sharedModules = lib.singleton {
+    programs.sioyek = {
+      enable = true;
+      inherit package;
+    };
+  };
+}

nix-community/stylix

commit c0be9011f30f820c1a727a0c2e763d87d86c9404

sioyek: remove unused lib module argument
Fixes: ec4d1ce9da22 ("sioyek: init (#2199)")

modules/sioyek/hm.nix
@@ -1,4 +1,4 @@
-{ mkTarget, lib, ... }:
+{ mkTarget, ... }:
 mkTarget {
   config =
     { colors }: