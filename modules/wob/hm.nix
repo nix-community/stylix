2964a6b47a206f0a2637d5ea8eead94f015c4392
octvs <octvs@posteo.de>
2026-02-25 15:54:42 +0100

    wob: support opacity

--- modules/wob/hm.nix
+++ modules/wob/hm.nix
@@ -1,12 +1,15 @@
-{ mkTarget, ... }:
+{ mkTarget, lib, ... }:
 mkTarget {
   config =
-    { colors }:
+    { colors, opacity }:
+    let
+      opacity' = lib.toHexString (builtins.ceil (opacity.popups * 255));
+    in
     {
       services.wob.settings = {
         "" = with colors; {
-          border_color = base05;
-          background_color = base00;
+          border_color = base05 + opacity';
+          background_color = base00 + opacity';
           bar_color = base0A;
           overflow_bar_color = base08;
           overflow_background_color = base00;
