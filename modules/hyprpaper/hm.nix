ea4112f613a7996ba9b169494849598b3af5ed79
Nuuvv <nuuvvtwitch@gmail.com>
2026-03-25 01:10:26 -0300

    Fixed formatting

--- modules/hyprpaper/hm.nix
+++ modules/hyprpaper/hm.nix
@@ -1,8 +1,5 @@
-{
-  lib,
-  mkTarget,
-  ...
-}:
+{ lib, mkTarget, ... }:
+
 mkTarget {
   options = {
     monitor = lib.mkOption {
@@ -11,16 +8,16 @@ mkTarget {
       description = "Monitor to apply the wallpaper to (\"\" matches all monitors). Example: \"DP-1\"";
     };
   };
-  config = {
-    image,
-    cfg,
-  }: {
-    services.hyprpaper.settings = {
-      wallpaper = lib.singleton {
-        monitor = cfg.monitor;
-        path = image;
+
+  config =
+    { image, cfg }:
+    {
+      services.hyprpaper.settings = {
+        wallpaper = lib.singleton {
+          inherit (cfg) monitor;
+          path = image;
+        };
+        splash = false;
       };
-      splash = false;
     };
-  };
 }
