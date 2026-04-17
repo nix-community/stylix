mightyiam/infra

commit ce740cf11253f7b733bb742c3f7865a9f57b2ce3

flake: update all inputs
Update all inputs and temporarily disable the upstream broken
aarch64-linux Blender testbed [1] ("Build failure: blender (on
aarch64-linux)").

[1]: https://github.com/NixOS/nixpkgs/issues/503387

Link: https://github.com/nix-community/stylix/pull/2159
Link: https://github.com/nix-community/stylix/pull/2180

Co-authored-by: NAHO <90870942+trueNAHO@users.noreply.github.com>
Co-authored-by: 0xda157 <da157@voidq.com>
Reviewed-by: NAHO <90870942+trueNAHO@users.noreply.github.com>

flake.nix
@@ -48,9 +48,10 @@
     };
 
     gnome-shell = {
-      # TODO: Unlocking the input requires updating the patch:
+      # TODO: Unlocking the input and pointing to official repository requires
+      # updating the patch:
       # https://github.com/nix-community/stylix/pull/224#discussion_r1460339607.
-      url = "gitlab:GNOME/gnome-shell/gnome-49?host=gitlab.gnome.org";
+      url = "github:GNOME/gnome-shell/ef02db02bf0ff342734d525b5767814770d85b49";
       flake = false;
     };
 

flake/dev/flake.nix
@@ -162,7 +162,13 @@
 
     noctalia-shell = {
       url = "github:noctalia-dev/noctalia-shell";
-      inputs.nixpkgs.follows = "dev-nixpkgs";
+      inputs = {
+        nixpkgs.follows = "dev-nixpkgs";
+        noctalia-qs.inputs = {
+          systems.follows = "dev-systems";
+          treefmt-nix.follows = "treefmt-nix";
+        };
+      };
     };
 
     nvf = {

modules/blender/testbeds/blender.nix
@@ -3,9 +3,17 @@ let
   package = pkgs.blender;
 in
 {
-  stylix.testbed.ui.application = {
-    name = "blender";
-    inherit package;
+  stylix.testbed = {
+    # TODO: Re-enable the aarch64-linux variant once the upstream build failure
+    # [1] ("Build failure: blender (on aarch64-linux)") is resolved.
+    #
+    # [1]: https://github.com/NixOS/nixpkgs/issues/503387
+    enable = pkgs.stdenv.hostPlatform.system != "aarch64-linux";
+
+    ui.application = {
+      name = "blender";
+      inherit package;
+    };
   };
 
   environment.systemPackages = [ package ];

flake/dev/pre-commit.nix
@@ -39,6 +39,7 @@
             enable = true;
             settings.config.default.extend-identifiers = lib.genAttrs [
               "MrSom3body"
+              "substituters"
             ] lib.id;
           };
           yamllint.enable = true;

generated/all-maintainers.nix
@@ -7,12 +7,6 @@
 #
 #       nix run .#all-maintainers
 {
-  "0x5a4" = {
-    email = "bej86nug@hhu.de";
-    github = "0x5a4";
-    githubId = 54070204;
-    name = "0x5a4";
-  };
   "0xda157" = {
     email = "da157@voidq.com";
     github = "0xda157";
@@ -117,6 +111,12 @@
     github = "dastarruer";
     githubId = 173855353;
     name = "dastarruer";
+  };
+  einetuer = {
+    email = "bej86nug@hhu.de";
+    github = "einetuer";
+    githubId = 54070204;
+    name = "0x5a4";
   };
   gideonwolfe = {
     email = "wolfegideon@gmail.com";

modules/sxiv/testbeds/sxiv.nix
@@ -5,7 +5,7 @@ in
 {
   stylix.testbed.ui.command.text = ''
     # Xresources isn't loaded by default, so we force it
-    ${lib.getExe pkgs.xorg.xrdb} ~/.Xresources
+    ${lib.getExe pkgs.xrdb} ~/.Xresources
 
     ${lib.getExe package} \
       "${pkgs.nixos-icons}/share/icons/hicolor/scalable/apps/nix-snowflake.svg";

modules/vscode/hm.nix
@@ -18,7 +18,7 @@ mkTarget {
       {
         warnings =
           lib.optional (config.programs.vscode.enable && cfg.profileNames == [ ])
-            ''stylix: vscode: `config.stylix.targets.vscode.profileNames` is empty. No theming will be applied. Add a profile or disable this warning by setting `stylix.targets.vscode.enable = false`.'';
+            "stylix: vscode: `config.stylix.targets.vscode.profileNames` is empty. No theming will be applied. Add a profile or disable this warning by setting `stylix.targets.vscode.enable = false`.";
       }
     )
     (

modules/zathura/hm.nix
@@ -9,10 +9,10 @@ mkTarget {
           getColorCh = colorName: channel: colors."${colorName}-rgb-${channel}";
           rgb =
             color:
-            ''rgb(${getColorCh color "r"}, ${getColorCh color "g"}, ${getColorCh color "b"})'';
+            "rgb(${getColorCh color "r"}, ${getColorCh color "g"}, ${getColorCh color "b"})";
           rgba =
             color: alpha:
-            ''rgba(${getColorCh color "r"}, ${getColorCh color "g"}, ${getColorCh color "b"}, ${toString alpha})'';
+            "rgba(${getColorCh color "r"}, ${getColorCh color "g"}, ${getColorCh color "b"}, ${toString alpha})";
         in
         {
           # Taken from here:

nix-community/stylix

commit cf650b21f8108b3f4071a9e2f61b7c1df95c88fc

blender/testbeds: re-enable temporarily disabled aarch64-linux variant
Re-enable the temporarily disabled upstream broken aarch64-linux Blender
testbed [1] ("Build failure: blender (on aarch64-linux)"), following
upstream commit [2] ("blender: fix build on aarch64-linux (missing
sse2neon dep)").

[1]: https://github.com/NixOS/nixpkgs/issues/503387
[2]: https://github.com/NixOS/nixpkgs/pull/510887

Fixes: ce740cf11253 ("flake: update all inputs")

flake.nix
@@ -2,7 +2,7 @@
   description = "Theming framework for NixOS, Home Manager, nix-darwin, and Nix-on-Droid";
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    nixpkgs.url = "github:imnotpoz/nixpkgs/blender-aarch64-linux";
 
     flake-parts = {
       url = "github:hercules-ci/flake-parts";

flake/dev/flake.nix
@@ -120,7 +120,7 @@
   #
   #       [1]: https://github.com/NixOS/nix/blob/d4f67fd46dfe2bc950bdfa14273f87b8a4c32e47/doc/manual/source/release-notes/rl-2.26.md?plain=1#L3-L11
   inputs = {
-    dev-nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    dev-nixpkgs.url = "github:imnotpoz/nixpkgs/blender-aarch64-linux";
 
     dev-flake-parts = {
       url = "github:hercules-ci/flake-parts";

modules/blender/testbeds/blender.nix
@@ -3,17 +3,9 @@ let
   package = pkgs.blender;
 in
 {
-  stylix.testbed = {
-    # TODO: Re-enable the aarch64-linux variant once the upstream build failure
-    # [1] ("Build failure: blender (on aarch64-linux)") is resolved.
-    #
-    # [1]: https://github.com/NixOS/nixpkgs/issues/503387
-    enable = pkgs.stdenv.hostPlatform.system != "aarch64-linux";
-
-    ui.application = {
-      name = "blender";
-      inherit package;
-    };
+  stylix.testbed.ui.application = {
+    name = "blender";
+    inherit package;
   };
 
   environment.systemPackages = [ package ];