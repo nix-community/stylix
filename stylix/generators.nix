9c325081dc43b099292a3239e6da57a89d782021
Louis Dalibard <louis.dalibard@gmail.com>
2026-04-24 21:08:17 +0200

    scheme-interoperability: flatten checks

--- stylix/generators.nix
+++ stylix/generators.nix
@@ -32,74 +32,76 @@ let
         "tonal-spot"
         "vibrant"
       ];
-    in
-    lib.throwIf (contrast < -1.0 || contrast > 1.0)
-      "stylix: matugen contrast must be between -1.0 and 1.0, got ${toString contrast}"
-      (
-        lib.throwIf (lightnessDark > 1.0)
-          "stylix: matugen lightnessDark must be <= 1.0, got ${toString lightnessDark}"
-          (
-            lib.throwIf (lightnessLight < -1.0)
-              "stylix: matugen lightnessLight must be >= -1.0, got ${toString lightnessLight}"
-              (
-                lib.throwIf (!lib.elem filter validFilters)
-                  "stylix: matugen filter must be one of ${lib.concatStringsSep ", " validFilters}, got ${filter}"
-                  (
-                    lib.throwIf (!lib.elem scheme validSchemes)
-                      "stylix: matugen scheme must be one of ${lib.concatStringsSep ", " validSchemes}, got ${scheme}"
-                      (
-                        polarity: image:
-                        let
-                          raw =
-                            pkgs.runCommand "palette-${name}-raw.json"
-                              {
-                                nativeBuildInputs = [ pkgs.matugen ];
-                                env = {
-                                  CONTRAST = toString contrast;
-                                  FILTER = filter;
-                                  LIGHTNESSDARK = toString lightnessDark;
-                                  LIGHTNESSLIGHT = toString lightnessLight;
-                                  SCHEME = "scheme-${scheme}";
-                                  POLARITY = polarity;
-                                  IMAGE = image;
-                                };
-                              }
-                              ''
-                                matugen \
-                                  --contrast "$CONTRAST" \
-                                  --lightness-dark "$LIGHTNESSDARK" \
-                                  --lightness-light "$LIGHTNESSLIGHT" \
-                                  --dry-run \
-                                  --include-image-in-json false \
-                                  --json strip \
-                                  --mode "$POLARITY" \
-                                  --resize-filter "$FILTER" \
-                                  --type "$SCHEME" \
-                                  --source-color-index 0 \
-                                  image \
-                                  "$IMAGE" \
-                                  | sed -E 's/"image":[[:space:]]*"[^"]*",?//g' \
-                                  > $out
-                              '';
-                        in
-                        pkgs.runCommand "palette-${name}.json"
-                          {
-                            nativeBuildInputs = [ pkgs.jq ];
-                            env = {
-                              POLARITY = polarity;
-                              RAW = raw;
-                            };
-                          }
-                          ''
-                            jq --arg polarity "$POLARITY" \
-                              '${jqSelector} | map_values(.[$polarity].color)' \
-                              "$RAW" > $out
-                          ''
-                      )
-                  )
-              )
+      validate = cond: msg: lib.throwIf cond "stylix: matugen ${msg}";
+      applyChecks =
+        value:
+        builtins.foldl' (acc: f: f acc) value [
+          (validate (
+            contrast < -1.0 || contrast > 1.0
+          ) "contrast must be between -1.0 and 1.0, got ${toString contrast}")
+          (validate (
+            lightnessDark > 1.0
+          ) "lightnessDark must be <= 1.0, got ${toString lightnessDark}")
+          (validate (
+            lightnessLight < -1.0
+          ) "lightnessLight must be >= -1.0, got ${toString lightnessLight}")
+          (validate (!lib.elem filter validFilters)
+            "filter must be one of ${lib.concatStringsSep ", " validFilters}, got ${filter}"
           )
-      );
+          (validate (!lib.elem scheme validSchemes)
+            "scheme must be one of ${lib.concatStringsSep ", " validSchemes}, got ${scheme}"
+          )
+        ];
+    in
+    applyChecks (
+      polarity: image:
+      let
+        raw =
+          pkgs.runCommand "palette-${name}-raw.json"
+            {
+              nativeBuildInputs = [ pkgs.matugen ];
+              env = {
+                CONTRAST = toString contrast;
+                FILTER = filter;
+                LIGHTNESSDARK = toString lightnessDark;
+                LIGHTNESSLIGHT = toString lightnessLight;
+                SCHEME = "scheme-${scheme}";
+                POLARITY = polarity;
+                IMAGE = image;
+              };
+            }
+            ''
+              matugen \
+                --contrast "$CONTRAST" \
+                --lightness-dark "$LIGHTNESSDARK" \
+                --lightness-light "$LIGHTNESSLIGHT" \
+                --dry-run \
+                --include-image-in-json false \
+                --json strip \
+                --mode "$POLARITY" \
+                --resize-filter "$FILTER" \
+                --type "$SCHEME" \
+                --source-color-index 0 \
+                image \
+                "$IMAGE" \
+                | sed -E 's/"image":[[:space:]]*"[^"]*",?//g' \
+                > $out
+            '';
+      in
+      pkgs.runCommand "palette-${name}.json"
+        {
+          nativeBuildInputs = [ pkgs.jq ];
+          env = {
+            POLARITY = polarity;
+            RAW = raw;
+          };
+        }
+        ''
+          jq --arg polarity "$POLARITY" \
+            '${jqSelector} | map_values(.[$polarity].color)' \
+            "$RAW" > $out
+        ''
+    );
 in
 {
   base16 = {
