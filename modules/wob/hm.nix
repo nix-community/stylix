ec58ac9c312aba9c8aa4ff76d333077b3a589578
Lyndon Sanche <lsanche@lyndeno.ca>
2026-05-12 17:34:44 -0600

    wob: Add opacity support

--- modules/wob/hm.nix
+++ modules/wob/hm.nix
@@ -1,16 +1,21 @@
-{ mkTarget, ... }:
+{ mkTarget, lib, ... }:
 mkTarget {
   config =
-    { colors }:
+    { colors, opacity }:
+    let
+      wobOpacity = lib.fixedWidthString 2 "0" (
+        lib.toHexString (builtins.ceil (opacity.popups * 255))
+      );
+    in
     {
       services.wob.settings = {
         "" = with colors; {
-          border_color = base05;
-          background_color = base00;
-          bar_color = base0A;
-          overflow_bar_color = base08;
-          overflow_background_color = base00;
-          overflow_border_color = base05;
+          border_color = "${base05}${wobOpacity}";
+          background_color = "${base00}${wobOpacity}";
+          bar_color = "${base0A}${wobOpacity}";
+          overflow_bar_color = "${base08}${wobOpacity}";
+          overflow_background_color = "${base00}${wobOpacity}";
+          overflow_border_color = "${base05}${wobOpacity}";
         };
       };
     };
