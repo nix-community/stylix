533482db9a5cf52a590632677e60094d6f44c3f9
reesilva <reesilva@proton.me>
2026-05-16 01:27:53 +0200

    hyprland: testbed for lua config
    this commit introduces the testbed for the lua config, even though it
    won't work until `hyprland:0.55` reaches `nixos-unstable`, which should
    be close given the following PR is already merged:
    https://github.com/NixOS/nixpkgs/pull/520059

---
+++ modules/hyprland/testbeds/hyprland-lua.nix
@@ -0,0 +1,17 @@
+{ lib, pkgs, ... }:
+{
+  stylix.testbed.ui = {
+    graphicalEnvironment = "hyprland";
+
+    # We need something to open a window so that we can check the window borders
+    application = {
+      name = "kitty";
+      package = pkgs.kitty;
+    };
+  };
+  home-manager.sharedModules = lib.singleton {
+    wayland.windowManager.hyprland = {
+      configType = "lua";
+    };
+  };
+}
