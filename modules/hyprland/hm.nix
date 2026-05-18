a0177f62a7883ed87a8f9cf402c4ffa27a7c7ce9
Jakku Night <J97k107u45k117n@gmail.com>
2026-05-18 14:07:37 -0300

    Running treefmt.

--- modules/hyprland/hm.nix
+++ modules/hyprland/hm.nix
@@ -20,29 +20,60 @@ mkTarget {
       { colors }:
       {
         wayland.windowManager.hyprland.settings =
-          let
-            rgb = color: "rgb(${color})";
-            rgba = color: alpha: "rgba(${color}${alpha})";
-          in
-          {
-            decoration.shadow.color = rgba colors.base00 "99";
-            general = {
-              "col.active_border" = rgb colors.base0D;
-              "col.inactive_border" = rgb colors.base03;
-            };
-            group = {
-              "col.border_inactive" = rgb colors.base03;
-              "col.border_active" = rgb colors.base0D;
-              "col.border_locked_active" = rgb colors.base0C;
+          if config.windowManager.hyprland.configType == "hyprlang" then
+            (
+              let
+                rgb = color: "rgb(${color})";
+                rgba = color: alpha: "rgba(${color}${alpha})";
+              in
+              {
+                decoration.shadow.color = rgba colors.base00 "99";
+                general = {
+                  "col.active_border" = rgb colors.base0D;
+                  "col.inactive_border" = rgb colors.base03;
+                };
+                group = {
+                  "col.border_inactive" = rgb colors.base03;
+                  "col.border_active" = rgb colors.base0D;
+                  "col.border_locked_active" = rgb colors.base0C;
 
-              groupbar = {
-                text_color = rgb colors.base05;
-                "col.active" = rgb colors.base0D;
-                "col.inactive" = rgb colors.base03;
-              };
-            };
-            misc.background_color = rgb colors.base00;
-          };
+                  groupbar = {
+                    text_color = rgb colors.base05;
+                    "col.active" = rgb colors.base0D;
+                    "col.inactive" = rgb colors.base03;
+                  };
+                };
+                misc.background_color = rgb colors.base00;
+              }
+            )
+          else
+            (
+              let
+                rgb = color: "rgb(${color})";
+                rgba = color: alpha: "rgba(${color}${alpha})";
+              in
+              {
+                config = {
+                  decoration.shadow.color = rgba colors.base00 "99";
+                  general = {
+                    "col.active_border" = rgb colors.base0D;
+                    "col.inactive_border" = rgb colors.base03;
+                  };
+                  group = {
+                    "col.border_inactive" = rgb colors.base03;
+                    "col.border_active" = rgb colors.base0D;
+                    "col.border_locked_active" = rgb colors.base0C;
+
+                    groupbar = {
+                      text_color = rgb colors.base05;
+                      "col.active" = rgb colors.base0D;
+                      "col.inactive" = rgb colors.base03;
+                    };
+                  };
+                  misc.background_color = rgb colors.base00;
+                };
+              }
+            );
       }
     )
     (
