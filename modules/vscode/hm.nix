8ed48a41087feeb66372ff718021a9512fc552b3
Thomas Bemme <14074615+thbemme@users.noreply.github.com>
2026-05-31 21:41:46 +0200

    vscode: support program.vscodium (#2331)
    Link: https://github.com/nix-community/stylix/pull/2331
    Closes: https://github.com/nix-community/stylix/issues/2299
    
    Reviewed-by: 0xda157 <da157@voidq.com>
    Tested-by: 0xda157 <da157@voidq.com>

--- modules/vscode/hm.nix
+++ modules/vscode/hm.nix
@@ -1,55 +1,13 @@
+{ lib, mkTarget, ... }:
 {
-  mkTarget,
-  pkgs,
-  config,
-  lib,
-  ...
-}:
-mkTarget {
-  options.profileNames = lib.mkOption {
-    description = "The VSCode profile names to apply styling on.";
-    type = lib.types.listOf lib.types.str;
-    default = [ "default" ];
-  };
-
-  config = [
-    (
-      { cfg }:
+  imports =
+    lib.mapAttrsToList
+      (
+        name: humanName:
+        lib.modules.importApply ./each-config.nix { inherit mkTarget name humanName; }
+      )
       {
-        warnings =
-          lib.optional (config.programs.vscode.enable && cfg.profileNames == [ ])
-            "stylix: vscode: `config.stylix.targets.vscode.profileNames` is empty. No theming will be applied. Add a profile or disable this warning by setting `stylix.targets.vscode.enable = false`.";
-      }
-    )
-    (
-      { cfg, colors }:
-      {
-        programs.vscode.profiles = lib.genAttrs cfg.profileNames (_: {
-          extensions = lib.singleton (
-            pkgs.runCommandLocal "stylix-vscode"
-              {
-                vscodeExtUniqueId = "stylix.stylix";
-                vscodeExtPublisher = "stylix";
-                version = "0.0.0";
-                theme = builtins.toJSON (import ./templates/theme.nix colors);
-                passAsFile = [ "theme" ];
-              }
-              ''
-                mkdir -p "$out/share/vscode/extensions/$vscodeExtUniqueId/themes"
-                ln -s ${./package.json} "$out/share/vscode/extensions/$vscodeExtUniqueId/package.json"
-                cp "$themePath" "$out/share/vscode/extensions/$vscodeExtUniqueId/themes/stylix.json"
-              ''
-          );
-        });
-      }
-    )
-    (
-      { cfg, fonts }:
-      {
-        programs.vscode.profiles = lib.genAttrs cfg.profileNames (_: {
-          userSettings = import ./templates/settings.nix fonts;
-        });
-      }
-    )
-  ];
+        "vscode" = "VSCode";
+        "vscodium" = "VSCodium";
+      };
 }
