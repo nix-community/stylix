769cc7503f6a2b8079dca294ea0b6d1460eb8e17
lwb <lwb-2021@qq.com>
2026-07-06 15:56:26 +0800

    avizo: use accent color

--- modules/avizo/hm.nix
+++ modules/avizo/hm.nix
@@ -13,7 +13,7 @@ mkTarget {
             in
             {
               background = "rgba(${base01-rgb-r}, ${base01-rgb-g}, ${base01-rgb-b}, ${aviOpacity})";
-              border-color = "rgba(${base0D-rgb-r}, ${base0D-rgb-g}, ${base0D-rgb-b}, ${aviOpacity})";
+              border-color = "rgba(${accent-rgb-r}, ${accent-rgb-g}, ${accent-rgb-b}, ${aviOpacity})";
               bar-fg-color = "rgba(${base05-rgb-r}, ${base05-rgb-g}, ${base05-rgb-b}, ${aviOpacity})";
               bar-bg-color = "rgba(${base01-rgb-r}, ${base01-rgb-g}, ${base01-rgb-b}, ${aviOpacity})";
               image-opacity = aviOpacity;
