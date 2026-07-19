2710ea623351616d0da102337e09b1840cca0f57
pancho horrillo <pancho@pancho.name>
2026-07-19 08:25:30 +0200

    {stylix,xresources}: gate home.pointerCursor.x11.enable
    Co-authored-by: 0xda157 <da157@voidq.com>

--- stylix/hm/cursor.nix
+++ stylix/hm/cursor.nix
@@ -20,7 +20,6 @@ in
           inherit (cfg) name package;
           enable = true;
           size = builtins.floor (cfg.size + 0.5);
-          x11.enable = true;
           gtk.enable = true;
         };
       };
