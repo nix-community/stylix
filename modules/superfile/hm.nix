ef49333194024f42778da0487dc3d0ab60d57a9e
reesilva <reesilva@proton.me>
2026-08-08 00:42:36 +0200

    superfile: fix the code to follow the guidelines

--- modules/superfile/hm.nix
+++ modules/superfile/hm.nix
@@ -1,57 +1,48 @@
 { mkTarget, ... }:
 mkTarget {
-  config = [
-    ({ colors, ... }: {
-      programs.superfile =
-        let
-          theme = "stylix";
-        in
-        {
-          settings.theme = "stylix";
-          themes.${theme} = {
-            code_syntax_highlight = "stylix";
-            full_screen_fg = colors.withHashtag.base05;
-            full_screen_bg = colors.withHashtag.base00;
-            gradient_color = [
-              colors.withHashtag.base0D
-              colors.withHashtag.base0E
-            ];
-            file_panel_fg = colors.withHashtag.base05;
-            file_panel_bg = colors.withHashtag.base00;
-            file_panel_border = colors.withHashtag.base03;
-            file_panel_border_active = colors.withHashtag.base16;
-            file_panel_top_directory_icon = colors.withHashtag.base14;
-            file_panel_top_path = colors.withHashtag.base0D;
-            file_panel_item_selected_fg = colors.withHashtag.base0D;
-            file_panel_item_selected_bg = colors.withHashtag.base00;
-            footer_fg = colors.withHashtag.base09;
-            footer_bg = colors.withHashtag.base00;
-            footer_border = colors.withHashtag.base03;
-            footer_border_active = colors.withHashtag.base16;
-            sidebar_fg = colors.withHashtag.base05;
-            sidebar_bg = colors.withHashtag.base00;
-            sidebar_title = colors.withHashtag.base14;
-            sidebar_border = colors.withHashtag.base03;
-            sidebar_border_active = colors.withHashtag.base0F;
-            sidebar_item_selected_fg = colors.withHashtag.base15;
-            sidebar_item_selected_bg = colors.withHashtag.base00;
-            sidebar_divider = colors.withHashtag.base03;
-            modal_fg = colors.withHashtag.base05;
-            modal_bg = colors.withHashtag.base00;
-            modal_border_active = colors.withHashtag.base14;
-            modal_cancel_fg = colors.withHashtag.base11;
-            modal_cancel_bg = colors.withHashtag.base13;
-            modal_confirm_fg = colors.withHashtag.base11;
-            modal_confirm_bg = colors.withHashtag.base14;
-            help_menu_hotkey = colors.withHashtag.base15;
-            help_menu_title = colors.withHashtag.base14;
-            cursor = colors.withHashtag.base13;
-            correct = colors.withHashtag.base0B;
-            error = colors.withHashtag.base0F;
-            hint = colors.withHashtag.base15;
-            cancel = colors.withHashtag.base13;
-          };
-        };
-    })
-  ];
+  config = { colors }: {
+    programs.superfile = {
+      settings.theme = "stylix";
+      themes.stylix = with colors.withHashtag; {
+        code_syntax_highlight = "stylix";
+        full_screen_fg = base05;
+        full_screen_bg = base00;
+        gradient_color = [ base0E ];
+        file_panel_fg = base05;
+        file_panel_bg = base00;
+        file_panel_border = base03;
+        file_panel_border_active = base16;
+        file_panel_top_directory_icon = base14;
+        file_panel_top_path = base0D;
+        file_panel_item_selected_fg = base0D;
+        file_panel_item_selected_bg = base00;
+        footer_fg = base09;
+        footer_bg = base00;
+        footer_border = base03;
+        footer_border_active = base16;
+        sidebar_fg = base05;
+        sidebar_bg = base00;
+        sidebar_title = base14;
+        sidebar_border = base03;
+        sidebar_border_active = base0F;
+        sidebar_item_selected_fg = base15;
+        sidebar_item_selected_bg = base00;
+        sidebar_divider = base03;
+        modal_fg = base05;
+        modal_bg = base00;
+        modal_border_active = base14;
+        modal_cancel_fg = base11;
+        modal_cancel_bg = base13;
+        modal_confirm_fg = base11;
+        modal_confirm_bg = base14;
+        help_menu_hotkey = base15;
+        help_menu_title = base14;
+        cursor = base13;
+        correct = base0B;
+        error = base0F;
+        hint = base15;
+        cancel = base13;
+      };
+    };
+  };
 }
--- modules/superfile/overlay.nix
+++ modules/superfile/overlay.nix
@@ -8,95 +8,97 @@ let
   inherit (lib) optionalAttrs;
   inherit (config.lib.stylix) colors;
 
-  chromaXml = pkgs.writeText "stylix-chroma.xml" ''
-    <style name="stylix">
-      <entry type="Background" style="bg:#${colors.base00}"/>
-      <entry type="LineHighlight" style="bg:#${colors.base02}"/>
-      <entry type="LineNumbers" style="#${colors.base04}"/>
-      <entry type="LineNumbersTable" style="#${colors.base04}"/>
-      <entry type="LineTableTD" style=""/>
-      <entry type="LineTable" style=""/>
-      <entry type="CodeLine" style="#${colors.base05}"/>
-      <entry type="Text" style="#${colors.base05}"/>
-      <entry type="Punctuation" style="#${colors.base05}"/>
-      <entry type="TextWhitespace" style="#${colors.base05}"/>
-      <entry type="Other" style="#${colors.base05}"/>
-      <entry type="Name" style="#${colors.base05}"/>
-      <entry type="NameOther" style="#${colors.base05}"/>
-      <entry type="NameConstant" style="#${colors.base0A}"/>
-      <entry type="NameDecorator" style="bold #${colors.base0D}"/>
-      <entry type="NameEntity" style="#${colors.base0C}"/>
-      <entry type="NameException" style="#${colors.base09}"/>
-      <entry type="NameNamespace" style="#${colors.base09}"/>
-      <entry type="Literal" style="#${colors.base05}"/>
-      <entry type="LiteralDate" style="#${colors.base05}"/>
-      <entry type="Generic" style="#${colors.base05}"/>
-      <entry type="GenericOutput" style="#${colors.base05}"/>
-      <entry type="GenericPrompt" style="#${colors.base05}"/>
-      <entry type="GenericStrong" style="#${colors.base05}"/>
-      <entry type="GenericTraceback" style="#${colors.base05}"/>
-      <entry type="Keyword" style="#${colors.base0E}"/>
-      <entry type="KeywordReserved" style="#${colors.base0E}"/>
-      <entry type="KeywordPseudo" style="#${colors.base0E}"/>
-      <entry type="KeywordConstant" style="#${colors.base09}"/>
-      <entry type="KeywordDeclaration" style="#${colors.base08}"/>
-      <entry type="KeywordNamespace" style="#${colors.base0C}"/>
-      <entry type="KeywordType" style="#${colors.base08}"/>
-      <entry type="NameFunction" style="#${colors.base0D}"/>
-      <entry type="NameFunctionMagic" style="#${colors.base0D}"/>
-      <entry type="NameAttribute" style="#${colors.base0D}"/>
-      <entry type="NameClass" style="#${colors.base0A}"/>
-      <entry type="NameBuiltin" style="#${colors.base0C}"/>
-      <entry type="NameBuiltinPseudo" style="#${colors.base0C}"/>
-      <entry type="NameLabel" style="#${colors.base0C}"/>
-      <entry type="NameVariable" style="#${colors.base05}"/>
-      <entry type="NameVariableClass" style="#${colors.base05}"/>
-      <entry type="NameVariableGlobal" style="#${colors.base05}"/>
-      <entry type="NameVariableInstance" style="#${colors.base05}"/>
-      <entry type="NameVariableMagic" style="#${colors.base05}"/>
-      <entry type="NameProperty" style="#${colors.base09}"/>
-      <entry type="NameTag" style="#${colors.base0E}"/>
-      <entry type="LiteralString" style="#${colors.base0B}"/>
-      <entry type="LiteralStringChar" style="#${colors.base0B}"/>
-      <entry type="LiteralStringSingle" style="#${colors.base0B}"/>
-      <entry type="LiteralStringDouble" style="#${colors.base0B}"/>
-      <entry type="LiteralStringBacktick" style="#${colors.base0B}"/>
-      <entry type="LiteralStringOther" style="#${colors.base0B}"/>
-      <entry type="LiteralStringSymbol" style="#${colors.base0B}"/>
-      <entry type="LiteralStringInterpol" style="#${colors.base0B}"/>
-      <entry type="LiteralStringHeredoc" style="#${colors.base04}"/>
-      <entry type="LiteralStringDoc" style="#${colors.base04}"/>
-      <entry type="LiteralStringEscape" style="#${colors.base0C}"/>
-      <entry type="LiteralStringRegex" style="#${colors.base0C}"/>
-      <entry type="LiteralStringDelimiter" style="#${colors.base0D}"/>
-      <entry type="LiteralStringAffix" style="#${colors.base0E}"/>
-      <entry type="LiteralNumber" style="#${colors.base09}"/>
-      <entry type="LiteralNumberBin" style="#${colors.base09}"/>
-      <entry type="LiteralNumberHex" style="#${colors.base09}"/>
-      <entry type="LiteralNumberInteger" style="#${colors.base09}"/>
-      <entry type="LiteralNumberFloat" style="#${colors.base09}"/>
-      <entry type="LiteralNumberIntegerLong" style="#${colors.base09}"/>
-      <entry type="LiteralNumberOct" style="#${colors.base09}"/>
-      <entry type="Operator" style="bold #${colors.base0C}"/>
-      <entry type="OperatorWord" style="bold #${colors.base0C}"/>
-      <entry type="Comment" style="italic #${colors.base03}"/>
-      <entry type="CommentSingle" style="italic #${colors.base03}"/>
-      <entry type="CommentMultiline" style="italic #${colors.base03}"/>
-      <entry type="CommentSpecial" style="italic #${colors.base03}"/>
-      <entry type="CommentHashbang" style="italic #${colors.base03}"/>
-      <entry type="CommentPreproc" style="italic #${colors.base03}"/>
-      <entry type="CommentPreprocFile" style="bold #${colors.base03}"/>
-      <entry type="Error" style="#${colors.base08}"/>
-      <entry type="GenericError" style="#${colors.base08}"/>
-      <entry type="GenericDeleted" style="#${colors.base08} bg:#${colors.base01}"/>
-      <entry type="GenericInserted" style="#${colors.base0B} bg:#${colors.base01}"/>
-      <entry type="GenericEmph" style="italic #${colors.base05}"/>
-      <entry type="GenericStrong" style="bold #${colors.base05}"/>
-      <entry type="GenericUnderline" style="underline #${colors.base05}"/>
-      <entry type="GenericHeading" style="bold #${colors.base09}"/>
-      <entry type="GenericSubheading" style="bold #${colors.base09}"/>
-    </style>
-  '';
+  chromaXml =
+    with colors.withHashtag;
+    pkgs.writeText "stylix-chroma.xml" ''
+      <style name="stylix">
+        <entry type="Background" style="bg:${base00}"/>
+        <entry type="LineHighlight" style="bg:${base02}"/>
+        <entry type="LineNumbers" style="${base04}"/>
+        <entry type="LineNumbersTable" style="${base04}"/>
+        <entry type="LineTableTD" style=""/>
+        <entry type="LineTable" style=""/>
+        <entry type="CodeLine" style="${base05}"/>
+        <entry type="Text" style="${base05}"/>
+        <entry type="Punctuation" style="${base05}"/>
+        <entry type="TextWhitespace" style="${base05}"/>
+        <entry type="Other" style="${base05}"/>
+        <entry type="Name" style="${base05}"/>
+        <entry type="NameOther" style="${base05}"/>
+        <entry type="NameConstant" style="${base0A}"/>
+        <entry type="NameDecorator" style="bold ${base0D}"/>
+        <entry type="NameEntity" style="${base0C}"/>
+        <entry type="NameException" style="${base09}"/>
+        <entry type="NameNamespace" style="${base09}"/>
+        <entry type="Literal" style="${base05}"/>
+        <entry type="LiteralDate" style="${base05}"/>
+        <entry type="Generic" style="${base05}"/>
+        <entry type="GenericOutput" style="${base05}"/>
+        <entry type="GenericPrompt" style="${base05}"/>
+        <entry type="GenericStrong" style="${base05}"/>
+        <entry type="GenericTraceback" style="${base05}"/>
+        <entry type="Keyword" style="${base0E}"/>
+        <entry type="KeywordReserved" style="${base0E}"/>
+        <entry type="KeywordPseudo" style="${base0E}"/>
+        <entry type="KeywordConstant" style="${base09}"/>
+        <entry type="KeywordDeclaration" style="${base08}"/>
+        <entry type="KeywordNamespace" style="${base0C}"/>
+        <entry type="KeywordType" style="${base08}"/>
+        <entry type="NameFunction" style="${base0D}"/>
+        <entry type="NameFunctionMagic" style="${base0D}"/>
+        <entry type="NameAttribute" style="${base0D}"/>
+        <entry type="NameClass" style="${base0A}"/>
+        <entry type="NameBuiltin" style="${base0C}"/>
+        <entry type="NameBuiltinPseudo" style="${base0C}"/>
+        <entry type="NameLabel" style="${base0C}"/>
+        <entry type="NameVariable" style="${base05}"/>
+        <entry type="NameVariableClass" style="${base05}"/>
+        <entry type="NameVariableGlobal" style="${base05}"/>
+        <entry type="NameVariableInstance" style="${base05}"/>
+        <entry type="NameVariableMagic" style="${base05}"/>
+        <entry type="NameProperty" style="${base09}"/>
+        <entry type="NameTag" style="${base0E}"/>
+        <entry type="LiteralString" style="${base0B}"/>
+        <entry type="LiteralStringChar" style="${base0B}"/>
+        <entry type="LiteralStringSingle" style="${base0B}"/>
+        <entry type="LiteralStringDouble" style="${base0B}"/>
+        <entry type="LiteralStringBacktick" style="${base0B}"/>
+        <entry type="LiteralStringOther" style="${base0B}"/>
+        <entry type="LiteralStringSymbol" style="${base0B}"/>
+        <entry type="LiteralStringInterpol" style="${base0B}"/>
+        <entry type="LiteralStringHeredoc" style="${base04}"/>
+        <entry type="LiteralStringDoc" style="${base04}"/>
+        <entry type="LiteralStringEscape" style="${base0C}"/>
+        <entry type="LiteralStringRegex" style="${base0C}"/>
+        <entry type="LiteralStringDelimiter" style="${base0D}"/>
+        <entry type="LiteralStringAffix" style="${base0E}"/>
+        <entry type="LiteralNumber" style="${base09}"/>
+        <entry type="LiteralNumberBin" style="${base09}"/>
+        <entry type="LiteralNumberHex" style="${base09}"/>
+        <entry type="LiteralNumberInteger" style="${base09}"/>
+        <entry type="LiteralNumberFloat" style="${base09}"/>
+        <entry type="LiteralNumberIntegerLong" style="${base09}"/>
+        <entry type="LiteralNumberOct" style="${base09}"/>
+        <entry type="Operator" style="bold ${base0C}"/>
+        <entry type="OperatorWord" style="bold ${base0C}"/>
+        <entry type="Comment" style="italic ${base03}"/>
+        <entry type="CommentSingle" style="italic ${base03}"/>
+        <entry type="CommentMultiline" style="italic ${base03}"/>
+        <entry type="CommentSpecial" style="italic ${base03}"/>
+        <entry type="CommentHashbang" style="italic ${base03}"/>
+        <entry type="CommentPreproc" style="italic ${base03}"/>
+        <entry type="CommentPreprocFile" style="bold ${base03}"/>
+        <entry type="Error" style="${base08}"/>
+        <entry type="GenericError" style="${base08}"/>
+        <entry type="GenericDeleted" style="${base08} bg:${base01}"/>
+        <entry type="GenericInserted" style="${base0B} bg:${base01}"/>
+        <entry type="GenericEmph" style="italic ${base05}"/>
+        <entry type="GenericStrong" style="bold ${base05}"/>
+        <entry type="GenericUnderline" style="underline ${base05}"/>
+        <entry type="GenericHeading" style="bold ${base09}"/>
+        <entry type="GenericSubheading" style="bold ${base09}"/>
+      </style>
+    '';
 
   registerGo = pkgs.writeText "register-stylix.go" ''
     package main
