df42e386735d8eecd846a05ab4ee3a6c2c7135f5
pancho horrillo <pancho@pancho.name>
2026-08-10 08:16:06 +0200

    xresources: merge into x11
    Co-authored-by: 0xda157 <da157@voidq.com>

--- modules/x11/hm.nix
+++ modules/x11/hm.nix
@@ -1,4 +1,58 @@
-{ mkTarget, ... }:
+{ mkTarget, lib, ... }:
 mkTarget {
-  config = [ ({ cursor }: { home.pointerCursor.x11.enable = true; }) ];
+  imports = [
+    (lib.mkRenamedOptionModuleWith {
+      from = [
+        "stylix"
+        "targets"
+        "xresources"
+      ];
+      sinceRelease = 2611;
+      to = [
+        "stylix"
+        "targets"
+        "x11"
+      ];
+    })
+  ];
+
+  config = [
+    ({ cursor }: { home.pointerCursor.x11.enable = true; })
+    ({ fonts }: {
+      xresources.properties = {
+        "*.faceName" = fonts.monospace.name;
+        "*.faceSize" = toString fonts.sizes.terminal;
+        "*.renderFont" = true;
+      };
+    })
+    ({ colors }: {
+      xresources.properties = with colors.withHashtag; {
+        "*foreground" = base05;
+        "*background" = base00;
+        "*cursorColor" = base05;
+        "*color0" = base00;
+        "*color1" = base08;
+        "*color2" = base0B;
+        "*color3" = base0A;
+        "*color4" = base0D;
+        "*color5" = base0E;
+        "*color6" = base0C;
+        "*color7" = base05;
+        "*color8" = base02;
+        "*color9" = base08;
+        "*color10" = base0B;
+        "*color11" = base0A;
+        "*color12" = base0D;
+        "*color13" = base0E;
+        "*color14" = base0C;
+        "*color15" = base07;
+        "*color16" = base09;
+        "*color17" = base0F;
+        "*color18" = base01;
+        "*color19" = base02;
+        "*color20" = base04;
+        "*color21" = base06;
+      };
+    })
+  ];
 }
--- modules/x11/meta.nix
+++ modules/x11/meta.nix
@@ -1,5 +1,8 @@
 { lib, ... }: {
   name = "X11";
   homepage = "https://www.x.org";
-  maintainers = [ lib.maintainers.panchoh ];
+  maintainers = with lib.maintainers; [
+    panchoh
+    zmberber
+  ];
 }
