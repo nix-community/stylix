nix-community/stylix

commit 74ee1ed5057e44edbcc36aa189a91d31eda60485

kubecolor: init (#657)
Link: https://github.com/danth/stylix/pull/657

Reviewed-by: NAHO <90870942+trueNAHO@users.noreply.github.com>

modules/kubecolor/hm.nix
@@ -1,0 +1,72 @@
+{ config, lib, ... }:
+{
+  options.stylix.targets.kubecolor.enable = config.lib.stylix.mkEnableTarget "kubecolor" true;
+
+  config = lib.mkIf config.stylix.targets.kubecolor.enable {
+    programs.kubecolor.settings = {
+      preset = if config.stylix.polarity == "either" then "" else "${config.stylix.polarity}";
+      theme = {
+        base = {
+          info = "fg=${config.lib.stylix.colors.withHashtag.base05-hex}";
+          primary = "fg=${config.lib.stylix.colors.withHashtag.base0E-hex}";
+          secondary = "fg=${config.lib.stylix.colors.withHashtag.base0D-hex}";
+          success = "fg=${config.lib.stylix.colors.withHashtag.base0B-hex}:bold";
+          warning = "fg=${config.lib.stylix.colors.withHashtag.base0A-hex}:bold";
+          danger = "fg=${config.lib.stylix.colors.withHashtag.base08-hex}:bold";
+          muted = "fg=${config.lib.stylix.colors.withHashtag.base04-hex}";
+          key = "fg=${config.lib.stylix.colors.withHashtag.base07-hex}:bold";
+        };
+        default = "fg=${config.lib.stylix.colors.withHashtag.base05-hex}";
+        data = {
+          key = "fg=${config.lib.stylix.colors.withHashtag.base07-hex}:bold";
+          string = "fg=${config.lib.stylix.colors.withHashtag.base05-hex}";
+          true = "fg=${config.lib.stylix.colors.withHashtag.base0B-hex}:bold";
+          false = "fg=${config.lib.stylix.colors.withHashtag.base08-hex}:bold";
+          number = "fg=${config.lib.stylix.colors.withHashtag.base0E-hex}";
+          null = "fg=${config.lib.stylix.colors.withHashtag.base04-hex}";
+          quantity = "fg=${config.lib.stylix.colors.withHashtag.base0E-hex}";
+          duration = "fg=${config.lib.stylix.colors.withHashtag.base09-hex}";
+          durationfresh = "fg=${config.lib.stylix.colors.withHashtag.base0B-hex}";
+          ratio = {
+            zero = "fg=${config.lib.stylix.colors.withHashtag.base04-hex}";
+            equal = "fg=${config.lib.stylix.colors.withHashtag.base0B-hex}";
+            unequal = "fg=${config.lib.stylix.colors.withHashtag.base0A-hex}";
+          };
+        };
+        status = {
+          success = "fg=${config.lib.stylix.colors.withHashtag.base0B-hex}:bold";
+          warning = "fg=${config.lib.stylix.colors.withHashtag.base0A-hex}:bold";
+          error = "fg=${config.lib.stylix.colors.withHashtag.base08-hex}:bold";
+        };
+        table = {
+          header = "fg=${config.lib.stylix.colors.withHashtag.base05-hex}:bold";
+          columns = "fg=${config.lib.stylix.colors.withHashtag.base05-hex}";
+        };
+        stderr = {
+          default = "fg=${config.lib.stylix.colors.withHashtag.base05-hex}";
+          error = "fg=${config.lib.stylix.colors.withHashtag.base08-hex}:bold";
+        };
+        describe = {
+          key = "fg=${config.lib.stylix.colors.withHashtag.base07-hex}:bold";
+        };
+        apply = {
+          created = "fg=${config.lib.stylix.colors.withHashtag.base0B-hex}";
+          configured = "fg=${config.lib.stylix.colors.withHashtag.base0A-hex}";
+          unchanged = "fg=${config.lib.stylix.colors.withHashtag.base05-hex}";
+          dryrun = "fg=${config.lib.stylix.colors.withHashtag.base0D-hex}";
+          fallback = "fg=${config.lib.stylix.colors.withHashtag.base05-hex}";
+        };
+        explain = {
+          key = "fg=${config.lib.stylix.colors.withHashtag.base07-hex}:bold";
+          required = "fg=${config.lib.stylix.colors.withHashtag.base00-hex}:bold";
+        };
+        options = {
+          flag = "fg=${config.lib.stylix.colors.withHashtag.base07-hex}:bold";
+        };
+        version = {
+          key = "fg=${config.lib.stylix.colors.withHashtag.base07-hex}:bold";
+        };
+      };
+    };
+  };
+}

commit 86f5edd657785242dfe15f37edef8613844b3126

kubecolor: omit preset instead of declaring empty string
Fixes: 74ee1ed5057e ("kubecolor: init (#657)")

modules/kubecolor/hm.nix
@@ -1,9 +1,8 @@
-{ mkTarget, ... }:
+{ mkTarget, lib, ... }:
 mkTarget {
   config = [
     ({ polarity }: {
-      programs.kubecolor.settings.preset =
-        if polarity == "either" then "" else polarity;
+      programs.kubecolor.settings.preset = lib.mkIf (polarity != "either") polarity;
     })
     ({ colors }: {
       programs.kubecolor.settings.theme = with colors.withHashtag; {