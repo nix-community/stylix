01e5fd0cc0fbb19b9852847fbba80c679068d591
DarkGui <darkgui@protonmail.com>
2026-08-16 12:45:57 -0300

    mango: document flake requirement

--- modules/mango/meta.nix
+++ modules/mango/meta.nix
@@ -2,4 +2,14 @@
   name = "Mango";
   homepage = "https://github.com/mangowm/mango";
   maintainers = [ lib.maintainers.darkguibrine ];
+
+  description = ''
+    Themes window borders, window state colors, overview jump mode, and the
+    monocle tab bar, plus their fonts.
+
+    This module requires the upstream Mango Home Manager module from the
+    `github:mangowm/mango` flake input, since it sets
+    `wayland.windowManager.mango.settings`, which is only available there and
+    not in nixpkgs.
+  '';
 }
