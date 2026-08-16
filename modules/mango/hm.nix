70663104aa1e07d482551fbba3cc65c9a7211d64
DarkGui <darkgui@protonmail.com>
2026-08-16 13:08:18 -0300

    mango: fix option detection for doc build

--- modules/mango/hm.nix
+++ modules/mango/hm.nix
@@ -5,43 +5,45 @@
   ...
 }:
 mkTarget {
-  config = lib.optionals (options.wayland.windowManager ? mango) [
-    ({ colors }: {
-      wayland.windowManager.mango.settings =
-        let
-          hex = color: "0x${color}ff";
-        in
-        {
-          rootcolor = hex colors.base00;
-          bordercolor = hex colors.base03;
-          dropcolor = "0x${colors.base00}55";
-          splitcolor = hex colors.base0D;
-          focuscolor = hex colors.base0D;
-          urgentcolor = hex colors.base08;
+  config =
+    lib.optionals (lib.hasAttrByPath [ "wayland" "windowManager" "mango" ] options)
+      [
+        ({ colors }: {
+          wayland.windowManager.mango.settings =
+            let
+              hex = color: "0x${color}ff";
+            in
+            {
+              rootcolor = hex colors.base00;
+              bordercolor = hex colors.base03;
+              dropcolor = "0x${colors.base00}55";
+              splitcolor = hex colors.base0D;
+              focuscolor = hex colors.base0D;
+              urgentcolor = hex colors.base08;
 
-          maximizescreencolor = hex colors.base0B;
-          scratchpadcolor = hex colors.base0C;
-          globalcolor = hex colors.base0E;
-          overlaycolor = hex colors.base0C;
+              maximizescreencolor = hex colors.base0B;
+              scratchpadcolor = hex colors.base0C;
+              globalcolor = hex colors.base0E;
+              overlaycolor = hex colors.base0C;
 
-          jump_label_decorate_fg_color = hex colors.base05;
-          jump_label_decorate_bg_color = hex colors.base01;
-          jump_label_decorate_focus_fg_color = hex colors.base00;
-          jump_label_decorate_focus_bg_color = hex colors.base0D;
-          jump_label_decorate_border_color = hex colors.base0D;
+              jump_label_decorate_fg_color = hex colors.base05;
+              jump_label_decorate_bg_color = hex colors.base01;
+              jump_label_decorate_focus_fg_color = hex colors.base00;
+              jump_label_decorate_focus_bg_color = hex colors.base0D;
+              jump_label_decorate_border_color = hex colors.base0D;
 
-          group_bar_decorate_fg_color = hex colors.base05;
-          group_bar_decorate_bg_color = hex colors.base01;
-          group_bar_decorate_focus_fg_color = hex colors.base00;
-          group_bar_decorate_focus_bg_color = hex colors.base0D;
-          group_bar_decorate_border_color = hex colors.base0D;
-        };
-    })
-    ({ fonts }: {
-      wayland.windowManager.mango.settings = {
-        jump_label_decorate_font_desc = "${fonts.sansSerif.name} Bold 16";
-        group_bar_decorate_font_desc = "${fonts.sansSerif.name} Bold 16";
-      };
-    })
-  ];
+              group_bar_decorate_fg_color = hex colors.base05;
+              group_bar_decorate_bg_color = hex colors.base01;
+              group_bar_decorate_focus_fg_color = hex colors.base00;
+              group_bar_decorate_focus_bg_color = hex colors.base0D;
+              group_bar_decorate_border_color = hex colors.base0D;
+            };
+        })
+        ({ fonts }: {
+          wayland.windowManager.mango.settings = {
+            jump_label_decorate_font_desc = "${fonts.sansSerif.name} Bold 16";
+            group_bar_decorate_font_desc = "${fonts.sansSerif.name} Bold 16";
+          };
+        })
+      ];
 }
