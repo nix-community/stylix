0ea72e37554116046bd7f157cfd29a4c63ba7afe
Lalit Yalamanchili <lalit.yalamanchili@gmail.com>
2026-08-22 16:46:25 +0530

    zed: support transparent background
    Apply Stylix desktop opacity to Zed's theme overrides so the editor
    background follows the configured desktop transparency. Make the
    editor surfaces transparent while retaining the themed base
    background and title bar when desktop opacity is enabled.

--- modules/zed/hm.nix
+++ modules/zed/hm.nix
@@ -1,4 +1,9 @@
-{ mkTarget, ... }:
+{
+  lib,
+  config,
+  mkTarget,
+  ...
+}:
 mkTarget {
   config = [
     ({ fonts }: {
@@ -11,14 +16,53 @@ mkTarget {
         };
       };
     })
-    ({ colors, inputs }: {
-      programs.zed-editor = {
-        userSettings.theme = "Base16 ${colors.scheme-name}";
-        themes.stylix = colors {
-          templateRepo = inputs.tinted-zed;
-          target = "base16";
+    (
+      {
+        inputs,
+        colors,
+        opacity,
+      }:
+      {
+        programs.zed-editor = {
+          userSettings = {
+            theme = "Base16 ${colors.scheme-name}";
+            "experimental.theme_overrides" =
+              with colors;
+              let
+                mkOpacityHexColor =
+                  color:
+                  let
+                    hex = builtins.substring 2 (-1) (
+                      config.lib.stylix.mkOpacityHexColor color opacity.desktop
+                    );
+                  in
+                  lib.toLower "${builtins.substring 2 (-1) hex}${builtins.substring 0 2 hex}";
+              in
+              if (opacity.desktop != 1.0) then
+                {
+                  "background" = "#${mkOpacityHexColor base00}";
+                  "surface.background" = "#${mkOpacityHexColor base00}";
+                  "title_bar.background" = "#${mkOpacityHexColor base00}";
+                  "title_bar.inactive.background" = "#00000000";
+                  "editor.background" = "#00000000";
+                  "editor.gutter.background" = "#00000000";
+                  "panel.background" = "#00000000";
+                  "toolbar.background" = "#00000000";
+                  "tab_bar.background" = "#00000000";
+                  "tab.active_background" = "#00000000";
+                  "tab.inactive_background" = "#00000000";
+                  "terminal.background" = "#00000000";
+
+                }
+              else
+                { };
+          };
+          themes.stylix = colors {
+            templateRepo = inputs.tinted-zed;
+            target = "base16";
+          };
         };
-      };
-    })
+      }
+    )
   ];
 }
