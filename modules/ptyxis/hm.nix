032b2ab3ef674c4d74be7ccd4193d24947ace783
First-Non-Interesting-Username <janekmusin@proton.me>
2026-08-23 10:00:09 +0200

    ptyxis: use system font

--- modules/ptyxis/hm.nix
+++ modules/ptyxis/hm.nix
@@ -39,15 +39,6 @@ mkTarget {
       }
     )
     (
-      { fonts }:
-      {
-        dconf.settings."org/gnome/Ptyxis" = {
-          use-system-font = false;
-          font-name = "${fonts.monospace.name} ${toString fonts.sizes.terminal}";
-        };
-      }
-    )
-    (
       { opacity, cfg }:
       {
         dconf.settings =
